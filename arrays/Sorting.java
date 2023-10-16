dd2846f2120d080e4d130d2815935ec7c9651470
Author Name <ronitnandwani@gmail.com>
2023-10-16 16:57:20 +0530

    Updated: Sorting

--- arrays/Sorting.java
+++ arrays/Sorting.java
@@ -1,7 +1,7 @@
 import java.util.Arrays;
 
 public class Sorting {
-    // Reverse an Array
+    // Reverse an Array - Not related to sorting
     public static void reverse(int[] array)
     {
 
@@ -22,6 +22,57 @@ public class Sorting {
             array[n - i - 1] = temp;
         }
     }
+
+
+    // Given an integer array A, find if an integer p exists in the array such that the number of integers greater than p in the array equals p.
+    public int nobleInteger(int[] A) {
+        int n = A.length ;
+
+        Arrays.sort(A);
+
+        for(int i = 0 ; i < n-1 ; i++)
+        {
+            if(A[i] != A[i+1])   // suppose A[] ={9,9,9,9,9,9,9,9,9} --> Inthis case there are no element is greater  , but if we only use the inner if condition then it return 1 which means there is present .
+            {                      //therefore we need this if condition
+                int count_element = n - 1 - i ;
+                if(A[i] == count_element)
+                {
+                    return 1 ;
+                }
+            }
+        }
+
+        int count_element = 0 ;         // edge case
+
+        if(A[n-1] == count_element)
+        {
+            return 1 ;
+        }
+
+        return -1 ;
+    }
+
+    // Find the Bth smallest element in given array A .
+    public int kthsmallest(final int[] a, int B) {
+        int n = a.length;
+        int minIndex,t;
+        for(int i=0;i<n;i++) {
+            minIndex = i;
+            for(int j=i+1;j<n;j++) {
+                if(a[j] < a[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            t = a[i];
+            a[i] = a[minIndex];
+            a[minIndex] = t;
+            if((i+1)==B) {
+                return a[i];
+            }
+        }
+        return 0;
+    }
+
 
     // Given an integer array A of size N. You can remove any element from the array in one operation.
     //The cost of this operation is the sum of all elements in the array present before this operation.
