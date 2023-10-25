7d978af900920dd8c51d4b4df74ce8cce52715d0
Author Name <ronitnandwani@gmail.com>
2023-10-25 16:59:22 +0530

    Added: Bit Manipulation Basics

---
+++ intermediate-dsa/BitManipulationBasics.java
@@ -0,0 +1,26 @@
+public class BitManipulation {
+    // Given two binary strings A and B. Return their sum (also a binary string).
+    public String addBinary(String A, String B) {
+        int carry=0;
+        int i=0;
+        String ans ="";
+        int alen=A.length();
+        int blen=B.length();
+        while(i<alen||i<blen||carry!=0)
+        {
+            int x=0;
+            if(i<alen && A.charAt(alen-1-i)=='1')
+                x=1;
+
+            int y=0;
+            if(i<blen && B.charAt(blen-1-i)=='1')
+                y=1;
+
+            ans = (x+y+carry)%2+ans;
+            carry=(x+y+carry)/2;
+            i++;
+        }
+
+        return ans;
+    }
+}
