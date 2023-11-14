7e6bd41d5238b18e51156d890ee42ed04b0ed2c5
Ronit Nandwani <ronitnandwani@gmail.com>
2023-11-14 10:51:36 +0530

    Modified: Intermediate Strings with all string assignments

--- intermediate/strings/Strings.java
+++ intermediate/strings/Strings.java
@@ -1,4 +1,87 @@
+package intermediate.strings;
+
 public class Strings {
+    // Given a string A of size N, find and return the longest palindromic substring in A.
+    //Substring of string A is A[i...j] where 0 <= i <= j < len(A)
+    //Palindrome string:
+    //A string which reads the same backwards. More formally, A is palindrome if reverse(A) = A.
+    //In case of conflict, return the substring which occurs first ( with the least starting index).
+    public static int expand(char s[], int c1, int c2) { // helper for longest palindromic
+        while(c1 >= 0 && c2<s.length && s[c1] == s[c2]) {
+            c1--;c2++;
+        }
+        return c2-c1-1;
+    }
+    public static String longestPalindrome(String A) {
+        int ans = 0;
+        String s = "";
+        char[] c = A.toCharArray();
+        int n = A.length();
+        for(int i=0;i<n;i++) {
+            int c1=i,c2=i;
+            int ac = expand(c,c1,c2);
+            if (ac > ans) {
+                ans = ac;
+                s = "";
+                int start = c1-ans/2,end = 1+c1+ans/2;
+                for(int j=start;j<end;j++) {
+                    s = s + c[j];
+                }
+            }
+        }
+        for(int i=0;i<n-1;i++) {
+            int c1=i,c2=i+1;
+            int ac = expand(c,c1,c2);
+            if (ac > ans) {
+                ans = ac;
+                s = "";
+                int start = 1+c1-ans/2,end = c2+ans/2;
+                for(int j=start;j<end;j++) {
+                    s = s + c[j];
+                }
+            }
+        }
+        return s;
+    }
+
+    // You are given a string A of size N.
+    //Return the string A after reversing the string word by word.
+    //NOTE:
+    //A sequence of non-space characters constitutes a word.
+    //Your reversed string should not contain leading or trailing spaces, even if it is present in the input string.
+    //If there are multiple spaces between words, reduce them to a single space in the reversed string.
+    public String solve(String A) {
+        String[] arr = A.split(" ");
+        String ans = "";
+        for(int i=arr.length-1;i>=0;i--) {
+            if(arr[i].length() > 0) {
+                if(ans.length() != 0) {
+                    ans = ans += " ";
+                }
+                ans = ans + arr[i];
+            }
+        }
+        return ans;
+    }
+
+    // Akash likes playing with strings. One day he thought of applying following operations on the string in the given order:
+    //Concatenate the string with itself.
+    //Delete all the uppercase letters.
+    //Replace each vowel with '#'.
+    //You are given a string A of size N consisting of lowercase and uppercase alphabets. Return the resultant string after applying the above operations.
+    //NOTE: 'a' , 'e' , 'i' , 'o' , 'u' are defined as vowels.
+    public String stringOperations(String A) {
+        String b = A.replaceAll("[A-Z]","");
+        char[] arr = b.toCharArray();
+        String ans = "";
+        for(int i=0;i<b.length();i++){
+            if (arr[i] == 'a' || arr[i] == 'e' || arr[i] == 'i' || arr[i] == 'o' || arr[i] == 'u') {
+                arr[i] = '#';
+            }
+        }
+        String c = String.valueOf(arr);
+        return c+c;
+    }
 
     // Given a string A, you are asked to reverse the string and return the reversed string.
 
@@ -48,5 +131,8 @@ public class Strings {
 //        return s;
     }
 
+    public static void main(String[] args) {
+        System.out.print(longestPalindrome("abb"));
+    }
 
 }
