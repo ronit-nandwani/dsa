feffcf3317da127c5c09181556c9fc18c55a26ca
Ronit Nandwani <ronitnandwani@gmail.com>
2023-11-17 16:16:58 +0530

    Updated: Advanced recursion added 1toA  and Ato1 problems

--- advanced/recursion/Recursion.java
+++ advanced/recursion/Recursion.java
@@ -1,6 +1,33 @@
 package advanced.recursion;
 
 public class Recursion {
+    // You are given an integer A, print 1 to A using using recursion.
+    // Note :- After printing all the numbers from 1 to A, print a new line.
+    public void print1toA(int A) {
+        if(A == 0) {
+            return;
+        } else {
+            // Print 1-A
+            System.out.print(A + " ");
+            print1toA(A-1);
+        }
+    }
+    // You are given an integer A, print A to 1 using using recursion.
+    // Note :- After printing all the numbers from A to 1, print a new line.
+    public void printAto1(int A) {
+        if(A == 0) {
+            return;
+        } else {
+            // Print A-1
+            System.out.print(A + " ");
+            printAto1(A-1);
+        }
+    }
+    public void solve(int A) {
+        // printAto1(A);
+        // print1toA(A);
+        System.out.println();
+    }
 
     // Write a program to find the factorial of the given number A using recursion.
     //Note: The factorial of a number N is defined as the product of the numbers from 1 to N.
