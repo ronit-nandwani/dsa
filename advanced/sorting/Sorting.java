5f115cd6530605583d03f43a221c3ea24cb7fcd2
Ronit Nandwani <ronitnandwani@gmail.com>
2023-12-20 09:24:37 +0530

    Modified: Sorting: Added problem to sort by factors using comparator

--- advanced/sorting/Sorting.java
+++ advanced/sorting/Sorting.java
@@ -1,8 +1,60 @@
 package advanced.sorting;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+
+
+class Solution {
+    public ArrayList<Integer> solve(ArrayList<Integer> A) {
+        Collections.sort(A, new SortByFactors());
+        return A;
+    }
+}
+
+class SortByFactors implements Comparator<Integer> {
+    public int compare(Integer a, Integer b) {
+        int factorsOfA = factors(a);
+        int factorsOfB = factors(b);
+
+        if (factorsOfA == factorsOfB) return a - b;
+
+        return factorsOfA - factorsOfB;
+    }
+
+    // Fast Factors find
+    public int factors(int N) {
+        int count=0;
+        for(int i=1;i*i<=N; i++){
+            if(N%i==0){
+                if(i==N/i){
+                    count++;
+                }
+                else{
+                    count= count+2;
+                }
+            }
+        }
+        return count;
+    }
+
+    // Slow factors find
+    // public int factors(int num) {
+    //     int i = 1, ans = 0;
+
+    //     while (i <= num) {
+    //         if (num % i == 0) ans += 2;
+    //         if (i == num) ans--;
+    //         i++;
+    //     }
+
+    //     return ans;
+    // }
+}
 
 public class Sorting {
+
     // Given an array A. Sort this array using Count Sort Algorithm and return the sorted array.
     public static int[] countSort(int[] A) {
         int n = A.length;
