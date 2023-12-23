01ce8fde5a1c9714ecf71197e338e9bab9f81faf
Ronit Nandwani <ronitnandwani@gmail.com>
2023-12-24 04:42:29 +0530

    Modified: searching 3 added problem to search in rotated array

--- advanced/searching/Searching2.java
+++ advanced/searching/Searching2.java
@@ -1,6 +1,66 @@
 package advanced.searching;
 
 public class Searching2 {
+    // Given a sorted array of integers A of size N and an integer B, 
+    // where array A is rotated at some pivot unknown beforehand.
+    // For example, the array [0, 1, 2, 4, 5, 6, 7] might become [4, 5, 6, 7, 0, 1, 2].
+    // Your task is to search for the target value B in the array. If found, return its index; otherwise, return -1.
+    // You can assume that no duplicates exist in the array.
+    // NOTE: You are expected to solve this problem with a time complexity of O(log(N)).
+    // Problem Constraints
+    // 1 <= N <= 1000000
+    // 1 <= A[i] <= 109
+    // All elements in A are Distinct.
+    // Input Format
+    // The First argument given is the integer array A.
+    // The Second argument given is the integer B.
+    // Output Format
+    // Return index of B in array A, otherwise return -1
+    // Example Input
+   //  Input 1:
+    // A = [4, 5, 6, 7, 0, 1, 2, 3]
+    // B = 4 
+    // Input 2:
+    // A : [ 9, 10, 3, 5, 6, 8 ]
+    // B : 5
+    // Example Output
+    // Output 1:
+    // 0 
+    // Output 2:
+    // 3
+    public int rotatedArrayArraySearch(final int[] A, int B) {
+        int n = A.length;
+        int l=0,r=n-1;
+        while(l<=r) {
+            int mid = l+(r-l)/2;
+            if(A[mid] == B) {
+                return mid;
+            }
+            if(B < A[0]) {
+                if(A[mid] < A[0]) {
+                    if(A[mid] < B) {
+                        l=mid+1;
+                    } else {
+                        r=mid-1;
+                    }
+                } else {
+                    l = mid+1;
+                }
+            } else {
+                if(A[mid] > A[0]) {
+                    if(A[mid] < B) {
+                        l=mid+1;
+                    } else {
+                        r=mid-1;
+                    }
+                } else {
+                    r=mid-1;
+                }
+            }
+        }
+        return -1;
+    }
+
     // Given an integer A. Compute and return the square root of A.
     //If A is not a perfect square, return floor(sqrt(A)).
     //NOTE:
