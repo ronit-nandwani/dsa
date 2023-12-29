1d7fcfb18660c947292b884a05b8107ed1cf3367
Ronit Nandwani <ronitnandwani@gmail.com>
2023-12-29 19:22:12 +0530

    Modified: hashing problem added for count pair difference

--- advanced/hashing/Hashing2.java
+++ advanced/hashing/Hashing2.java
@@ -4,6 +4,55 @@ import java.util.HashMap;
 import java.util.HashSet;
 
 public class Hashing2 {
+    // You are given an array A of N integers and an integer B.
+    // Count the number of pairs (i,j) such that A[i] - A[j] = B and i ≠ j.
+    // Since the answer can be very large, return the remainder after dividing the count with 109+7.
+    // Problem Constraints
+    // 1 <= N <= 105
+    // 1 <= A[i] <= 109
+    // 1 <= B <= 109
+    // Input Format
+    // First argument A is an array of integers and second argument B is an integer.
+    // Output Format
+    // Return an integer.
+    // Example Input
+    // Input 1:
+    // A = [3, 5, 1, 2]
+    // B = 4
+    // Input 2:
+    // A = [1, 2, 1, 2]
+    // B = 1
+    // Example Output
+    // Output 1:
+    // 1
+    // Output 2:
+    // 4
+    // Example Explanation
+    // Example 1:
+    // The only pair is (2, 3) which gives difference as 4
+    // Example 2:
+    // The pair which gives difference as 3 are (2, 1), (4, 1), (2, 3) and (4, 3).
+
+    public int countPairDifference(int[] A, int k) {
+        int ans = 0;
+        int n = A.length;
+        HashMap<Integer,Integer> hm = new HashMap<Integer,Integer>();
+        for(int i=0;i<n;i++) {
+            if(hm.containsKey(A[i]-k)) {
+                ans += hm.get(A[i]-k);
+            }
+            if(hm.containsKey(A[i]+k)) {
+                ans += hm.get(A[i]+k);
+            }
+            if(hm.containsKey(A[i])) {
+                hm.put(A[i],hm.get(A[i])+1);
+            } else {
+                hm.put(A[i],1);
+            }
+        }
+        return ans%1000000007;
+    }
+
     // You are given an array of N integers, A1, A2 ,..., AN and an integer B. Return the of count of distinct numbers in all windows of size B.
     // Formally, return an array of size N-B+1 where i'th element in this array contains number of distinct elements in sequence Ai, Ai+1 ,..., Ai+B-1.
     // NOTE: if B > N, return an empty array.
