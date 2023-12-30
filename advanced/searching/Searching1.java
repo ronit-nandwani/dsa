53f726f67943ce343762c7902dee4d68e46a27af
Ronit Nandwani <ronitnandwani@gmail.com>
2023-12-30 18:35:59 +0530

    Modified: searching 1 added problem to find index of element in sorted array

--- advanced/searching/Searching1.java
+++ advanced/searching/Searching1.java
@@ -3,6 +3,53 @@ package advanced.searching;
 import java.util.Arrays;
 
 public class Searching1 {
+    // You are given a sorted array A of size N and a target value B.
+    // Your task is to find the index (0-based indexing) of the target value in the array.
+    // If the target value is present, return its index.
+    // If the target value is not found, return the index of least element greater than equal to B.
+    // Your solution should have a time complexity of O(log(N)).
+    // Problem Constraints
+    // 1 <= N <= 105
+    // 1 <= A[i] <= 105
+    // 1 <= B <= 105
+    // Input Format
+    // The first argument is an integer array A of size N.
+    // The second argument is an integer B.
+    // Output Format
+    // Return an integer denoting the index of target value.
+    // Example Input
+    // Input 1:
+    // A = [1, 3, 5, 6]
+    // B = 5 
+    // Input 2:
+    // A = [1, 4, 9]
+    // B = 3
+    // Example Output
+    // Output 1:
+    // 2 
+    // Output 2:
+    // 1
+    // Example Explanation
+    // Explanation 1:
+    // The target value is present at index 2. 
+    // Explanation 2:
+    // The target value should be inserted at index 1.
+    public int sortedInsertPosition(int[] A, int B) {
+        int n = A.length;
+        int l=0,r=n-1;
+        while(l<=r) {
+            int mid = (l+r)/2;
+            if(A[mid] == B) {
+                return mid;
+            }
+            if(B<A[mid]) {
+                r = mid - 1;
+            } else {
+                l=mid+1;
+            }
+        }
+        return l;
+    }
     // Given a sorted array of integers A (0-indexed) of size N, find the left most and the right most index of a given integer B in the array A.
     //Return an array of size 2, such that
     //          First element = Left most index of B in A
