8fa92088eb63d5e954cfa7568ac811a639f1d836
Ronit Nandwani <ronitnandwani@gmail.com>
2023-12-31 07:36:00 +0100

    Modified: searching 1 added problem single element in sorted array

--- advanced/searching/Searching1.java
+++ advanced/searching/Searching1.java
@@ -3,6 +3,55 @@ package advanced.searching;
 import java.util.Arrays;
 
 public class Searching1 {
+    // Given a sorted array of integers A where every element appears twice except for one element which appears once, find and return this single element that appears only once.
+    // Elements which are appearing twice are adjacent to each other.
+    // NOTE: Users are expected to solve this in O(log(N)) time.
+    // Problem Constraints
+    // 1 <= |A| <= 100000
+    // 1 <= A[i] <= 10^9
+    // Input Format
+    // The only argument given is the integer array A.
+    // Output Format
+    // Return the single element that appears only once.
+    // Example Input
+    // Input 1:
+    // A = [1, 1, 7]
+    // Input 2:
+    // A = [2, 3, 3]
+    // Example Output
+    // Output 1:
+    // 7
+    // Output 2:
+    // 2
+    // Example Explanation
+    // Explanation 1:
+    // 7 appears once
+    // Explanation 2:
+    // 2 appears once
+    public int singleElementinSortedArray(int[] A) {
+        int n = A.length;
+        int l=0,r=n-1;
+        while(l<=r) {
+            int mid = (l+r)/2;
+            if((mid==0||A[mid-1]!=A[mid])&&(mid==n-1 || A[mid+1]!=A[mid])) {
+                return A[mid];
+            }
+            if(mid==0 || A[mid-1]!=A[mid]) {
+                if(mid%2 == 0) {
+                    l=mid+1;
+                } else {
+                    r=mid-1;
+                }
+            } else {
+                if(mid%2 != 0) {
+                    l=mid+1;
+                } else {
+                    r=mid-1;
+                }
+            }
+        }
+        return -1;
+    }
     // You are given a sorted array A of size N and a target value B.
     // Your task is to find the index (0-based indexing) of the target value in the array.
     // If the target value is present, return its index.
