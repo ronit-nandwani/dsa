c5bd99550a59a9b84994e0153b5b83a0a26f98ec
Ronit Nandwani <ronitnandwani@gmail.com>
2024-01-02 19:47:37 +0530

    Modified: searching 2 find median in sorted arrays problem added

--- advanced/searching/Searching2.java
+++ advanced/searching/Searching2.java
@@ -1,6 +1,66 @@
 package advanced.searching;
 
+import java.util.List;
+
 public class Searching2 {
+    // There are two sorted arrays A and B of sizes N and M respectively.
+    //Find the median of the two sorted arrays ( The median of the array formed by merging both the arrays ).
+    //NOTE:
+    //The overall run time complexity should be O(log(m+n)).
+    //IF the number of elements in the merged array is even, then the median is the average of (n/2)th and (n/2+1)th element. For example, if the array is [1 2 3 4], the median is (2 + 3) / 2.0 = 2.5.
+    //Problem Constraints
+    //1 <= N + M <= 2*106
+    //Input Format
+    //The first argument is an integer array A of size N.
+    //The second argument is an integer array B of size M.
+    //Output Format
+    //Return a decimal value denoting the median of two sorted arrays.
+    //Example Input
+    //Input 1:
+    // A = [1, 4, 5]
+    // B = [2, 3]
+    //Input 2:
+    // A = [1, 2, 3]
+    // B = [4]
+    //Example Output
+    //Output 1:
+    // 3.0
+    //Output 2:
+    // 2.5
+    //Example Explanation
+    //Explanation 1:
+    // The median of both the sorted arrays will be 3.0.
+    //Explanation 2:
+    // The median of both the sorted arrays will be (2+3)/2 = 2.5.
+    public double findMedianSortedArrays(final List<Integer> a, final List<Integer> b) {
+        int n = a.size();
+        int m = b.size();
+        if(n>m) {
+            return findMedianSortedArrays(b,a);
+        }
+        int l=0,r=n;
+        while(l<=r) {
+            int midA = (l+r)/2;
+            int midB = (n+m+1)/2 - midA;
+            int maxLeftA = (midA==0) ? Integer.MIN_VALUE : a.get(midA-1);
+            int minRightA = (midA==n) ? Integer.MAX_VALUE : a.get(midA);
+            int maxLeftB = (midB==0) ? Integer.MIN_VALUE : b.get(midB-1);
+            int minRightB = (midB==m) ? Integer.MAX_VALUE : b.get(midB);
+            if(maxLeftA <= minRightB && maxLeftB <= minRightA) {
+                if((n+m)%2==0) {
+                    return (double)(Math.max(maxLeftA,maxLeftB) + Math.min(minRightA,minRightB))/2;
+                } else {
+                    return (double)Math.max(maxLeftA,maxLeftB);
+                }
+            }
+            if(maxLeftA > minRightB) {
+                r=midA-1;
+            } else {
+                l=midA+1;
+            }
+        }
+        return -1;
+    }
     // Given a sorted array of integers A of size N and an integer B, 
     // where array A is rotated at some pivot unknown beforehand.
     // For example, the array [0, 1, 2, 4, 5, 6, 7] might become [4, 5, 6, 7, 0, 1, 2].
