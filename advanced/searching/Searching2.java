48b1b5aa060813ad260d77c05ec0ba8d69e0bf1a
Ronit Nandwani <ronitnandwani@gmail.com>
2024-01-08 21:47:26 +0530

    Modified: Searching Ath Magical problem added

--- advanced/searching/Searching2.java
+++ advanced/searching/Searching2.java
@@ -3,6 +3,68 @@ package advanced.searching;
 import java.util.List;
 
 public class Searching2 {
+    // Ath Magical Number
+    // You are given three positive integers, A, B, and C.
+    //Any positive integer is magical if divisible by either B or C.
+    //Return the Ath smallest magical number. Since the answer may be very large, return modulo 109 + 7.
+    //Note: Ensure to prevent integer overflow while calculating.
+    //Problem Constraints
+    //1 <= A <= 109
+    //2 <= B, C <= 40000
+    //Input Format
+    //The first argument given is an integer A.
+    //The second argument given is an integer B.
+    //The third argument given is an integer C.
+    //Output Format
+    //Return the Ath smallest magical number. Since the answer may be very large, return modulo 109 + 7.
+    //Example Input
+    //Input 1:
+    // A = 1
+    // B = 2
+    // C = 3
+    //Input 2:
+    // A = 4
+    // B = 2
+    // C = 3
+    //Example Output
+    //Output 1:
+    // 2
+    //Output 2:
+    // 6
+    //Example Explanation
+    //Explanation 1:
+    // 1st magical number is 2.
+    //Explanation 2:
+    // First four magical numbers are 2, 3, 4, 6 so the 4th magical number is 6.
+    public static int gcd(int a, int b) {
+
+        // if b=0, a is the GCD
+        if (b == 0)
+            return a;
+
+            // call the gcd() method recursively by
+            // replacing a with b and b with
+            // modulus(a,b) as long as b != 0
+        else
+            return gcd(b, a % b);
+    }
+    public static int AthMagicalNumber(int A, int B, int C) {
+        long lcm = ((long) B *C)/gcd(B,C);
+        long l = Math.min(B,C), r= (long) A *Math.min(B,C);
+        while(l<=r) {
+            long mid = (l+r)/2;
+            long count = mid/B + mid/C - mid/lcm;
+            if((mid%B==0||mid%C==0) && count==A) {
+                return (int)(mid%1000000007);
+            }
+            if(count<A) {
+                l=mid+1;
+            } else {
+                r=mid-1;
+            }
+        }
+        return -1;
+    }
     // There are two sorted arrays A and B of sizes N and M respectively.
     //Find the median of the two sorted arrays ( The median of the array formed by merging both the arrays ).
     //NOTE:
@@ -164,7 +226,10 @@ public class Searching2 {
         return r;
     }
     public static void main(String[] args) {
-        // expected output: 46340
-        System.out.println(sqrt(2147483647));
+//        // expected output: 46340
+//        System.out.println(sqrt(2147483647));
+
+        // Expected Output: 238134151
+        System.out.println(AthMagicalNumber(807414236, 3788, 38141));
     }
 }
