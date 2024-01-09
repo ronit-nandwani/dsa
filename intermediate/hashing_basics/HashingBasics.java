29d80c23af3b9f441d6908c213df5b4c5179be4d
Ronit Nandwani <ronitnandwani@gmail.com>
2024-01-09 13:36:09 +0530

    Modified: hashing problem added find common elements

--- intermediate/hashing_basics/HashingBasics.java
+++ intermediate/hashing_basics/HashingBasics.java
@@ -3,6 +3,60 @@ package intermediate.hashing_basics;
 import java.util.*;
 
 public class HashingBasics {
+    //Common Elements
+    // Given two integer arrays, A and B of size N and M, respectively. Your task is to find all the common elements in both the array.
+    //NOTE:
+    //Each element in the result should appear as many times as it appears in both arrays.
+    //The result can be in any order.
+    //Problem Constraints
+    //1 <= N, M <= 105
+    //1 <= A[i] <= 109
+    //Input Format
+    //First argument is an integer array A of size N.
+    //Second argument is an integer array B of size M.
+    //Output Format
+    //Return an integer array denoting the common elements.
+    //Example Input
+    //Input 1:
+    // A = [1, 2, 2, 1]
+    // B = [2, 3, 1, 2]
+    //Input 2:
+    // A = [2, 1, 4, 10]
+    // B = [3, 6, 2, 10, 10]
+    //Example Output
+    //Output 1:
+    // [1, 2, 2]
+    //Output 2:
+    // [2, 10]
+    //Example Explanation
+    //Explanation 1:
+    // Elements (1, 2, 2) appears in both the array. Note 2 appears twice in both the array.
+    //Explantion 2:
+    // Elements (2, 10) appears in both the array.
+    public ArrayList<Integer> commonElements(ArrayList<Integer> A, ArrayList<Integer> B) {
+        // Just write your code below to complete the function. Required input is available to you as the function arguments.
+        // Do not print the result or any output. Just return the result via this function.
+        Map<Integer,Integer> hm=new HashMap<>();
+
+        for(int i=0;i<A.size();i++){
+            hm.put(A.get(i),hm.getOrDefault(A.get(i),0)+1);
+        }
+
+        ArrayList<Integer> ans=new ArrayList<>();
+
+        for(int i=0;i<B.size();i++){
+            if(hm.containsKey(B.get(i))){
+                ans.add(B.get(i));
+                if(hm.get(B.get(i))==1){
+                    hm.remove(B.get(i));
+                    continue;
+                }
+                hm.put(B.get(i),hm.getOrDefault(B.get(i),0)-1);
+            }
+        }
+
+        return ans;
+    }
     // Find the count of the sub arrays in the array which sums to zero. Since the answer can be very large, return the remainder on dividing the result with 109+7
     public int countSubArrayWithZeroSum(int[] A) {
         int n = A.length;
