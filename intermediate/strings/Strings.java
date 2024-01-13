c1ba5c1b597e673de17633d0f3d928f6080b44a5
Ronit Nandwani <ronitnandwani@gmail.com>
2024-01-13 07:36:00 +0100

    Modified: Strings added problem to check anagrams

--- intermediate/strings/Strings.java
+++ intermediate/strings/Strings.java
@@ -1,6 +1,67 @@
 package intermediate.strings;
 
 public class Strings {
+    // You are given two lowercase strings A and B each of length N. Return 1 if they are anagrams to each other and 0 if not.
+    //Note : Two strings A and B are called anagrams to each other if A can be formed after rearranging the letters of B.
+    //Problem Constraints
+    //1 <= N <= 105
+    //A and B are lowercase strings
+    //Input Format
+    //Both arguments A and B are a string.
+    //Output Format
+    //Return 1 if they are anagrams and 0 if not
+    //Example Input
+    //Input 1:
+    //A = "cat"
+    //B = "bat"
+    //Input 2:
+    //A = "secure"
+    //B = "rescue"
+    //Example Output
+    //Output 1:
+    //0
+    //Output 2:
+    //1
+    //Example Explanation
+    //For Input 1:
+    //The words cannot be rearranged to form the same word. So, they are not anagrams.
+    //For Input 2:
+    //They are an anagram.
+    public int checkAnagrams(String A, String B) {
+        //Optimized
+        int[] freq1 = new int[26];
+        int[] freq2 = new int[26];
+        for(int i = 0 ; i < A.length() ; i++){
+            freq1[A.charAt(i) - 'a']++;
+            freq2[B.charAt(i) - 'a']++;
+        }
+        for(int i = 0 ; i < 26 ; i++){
+            if(freq1[i] != freq2[i]){
+                return 0;
+            }
+        }
+        return 1;
+
+
+        // Not Optimized
+        // int countA = 0;
+        // int countB = 0;
+
+        // int n = A.length();
+
+        // for(int i=0; i<n; i++)
+        // {
+        //     countA = countA + A.charAt(i);
+        //     countB = countB + B.charAt(i);
+        // }
+
+        // if(countA == countB)
+        // {
+        //     return 1;
+        // }
+
+        // return 0;
+    }
     // Given a string A of size N, find and return the longest palindromic substring in A.
     //Substring of string A is A[i...j] where 0 <= i <= j < len(A)
     //Palindrome string:
