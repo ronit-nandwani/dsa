b9bf03e10989ff50792c17c61ccb6afd98f3afbd
Ronit Nandwani <ronitnandwani@gmail.com>
2024-01-15 19:21:39 +0530

    Modified: sorting added merge two sub arrays

--- advanced/sorting/Sorting.java
+++ advanced/sorting/Sorting.java
@@ -54,6 +54,59 @@ class SortByFactors implements Comparator<Integer> {
 }
 
 public class Sorting {
+    // Merge Two Sorted Arrays
+    // Given two sorted integer arrays A and B, merge B and A as one sorted array and return it as an output.
+    //Note: A linear time complexity is expected and you should avoid use of any library function.
+    //Problem Constraints
+    //-2×109 <= A[i], B[i] <= 2×109
+    //1 <= |A|, |B| <= 5×104
+    //Input Format
+    //First Argument is a 1-D array representing  A.
+    //Second Argument is also a 1-D array representing B.
+    //Output Format
+    //Return a 1-D vector which you got after merging A and B.
+    //Example Input
+    //Input 1:
+    //A = [4, 7, 9]
+    //B = [2, 11, 19]
+    //Input 2:
+    //A = [1]
+    //B = [2]
+    //Example Output
+    //Output 1:
+    //[2, 4, 7, 9, 11, 19]
+    //Output 2:
+    //[1, 2]
+    //Example Explanation
+    //Explanation 1:
+    //Merging A and B produces the output as described above.
+    //Explanation 2:
+    // Merging A and B produces the output as described above.
+    public int[] mergeTwoSortedArrays(final int[] A, final int[] B) {
+        int n = A.length;
+        int m = B.length;
+        int[] ans = new int[n+m];
+        int i=0, j=0, k=0;
+        while(i<n && j<m) {
+            if(A[i] <= B[j]) {
+                ans[k] = A[i];
+                i++;
+            } else {
+                ans[k] = B[j];
+                j++;
+            }
+            k++;
+        }
+        while(i<n) {
+            ans[k] = A[i];
+            k++;i++;
+        }
+        while(j<m) {
+            ans[k] = B[j];
+            k++;j++;
+        }
+        return ans;
+    }
 
     // Given an array A. Sort this array using Count Sort Algorithm and return the sorted array.
     public static int[] countSort(int[] A) {
@@ -74,8 +127,8 @@ public class Sorting {
     }
 
     public static void main(String[] args) {
-        int[] arr = {6,3,3,6,7,8,7,3,7};
-        // Expected Output: [3,3,3,6,6,7,7,7,8]
-        System.out.println(Arrays.toString(countSort(arr)));
+       int[] arr = {6,3,3,6,7,8,7,3,7};
+       // Expected Output: [3,3,3,6,6,7,7,7,8]
+       System.out.println(Arrays.toString(countSort(arr)));
     }
 }
