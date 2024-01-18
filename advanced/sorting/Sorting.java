0e650d296a3a613e95c22a3311fc983c3b1440f8
Ronit Nandwani <ronitnandwani@gmail.com>
2024-01-18 07:36:00 +0100

    Modified: Sorting added problem for forming largest number using comparator

--- advanced/sorting/Sorting.java
+++ advanced/sorting/Sorting.java
@@ -1,3 +1,4 @@
+// Modified: sorting added form largest number problem by soring
 package advanced.sorting;
 
 import java.util.ArrayList;
@@ -54,6 +55,54 @@ class SortByFactors implements Comparator<Integer> {
 }
 
 public class Sorting {
+
+    // Form largest number
+    // Given an array A of non-negative integers, arrange them such that they form the largest number.
+    //Note: The result may be very large, so you need to return a string instead of an integer.
+    //Problem Constraints
+    //1 <= len(A) <= 100000
+    //0 <= A[i] <= 2*109
+    //Input Format
+    //The first argument is an array of integers.
+    //Output Format
+    //Return a string representing the largest number.
+    //Example Input
+    //Input 1:
+    // A = [3, 30, 34, 5, 9]
+    //Input 2:
+    // A = [2, 3, 9, 0]
+    //Example Output
+    //Output 1:
+    // "9534330"
+    //Output 2:
+    // "9320"
+    //Example Explanation
+    //Explanation 1:
+    //Reorder the numbers to [9, 5, 34, 3, 30] to form the largest number.
+    //Explanation 2:
+    //Reorder the numbers to [9, 3, 2, 0] to form the largest number 9320.
+    public String largestNumber(ArrayList<Integer> A) {
+        Collections.sort(A, new Comparator<Integer>() {
+            @Override
+            public int compare(Integer f,Integer s)
+            {
+                String sf = String.valueOf(f);
+                String ss = String.valueOf(s);
+                String s1 = sf + ss;
+                String s2 = ss + sf;
+                return s2.compareTo(s1);
+            }
+        });
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < A.size(); i++) {
+            sb.append(Integer.toString(A.get(i)));
+        }
+        if(A.get(0) == 0) {
+            return "0";
+        }
+        return sb.toString();
+    }
+
     static long ic_c = 0;
     static long mod = 1000000000 + 7;
 
@@ -65,7 +114,7 @@ public class Sorting {
                 ans[k] = A[i];
                 i++;
             } else {
-                ic = (long)(ic + mid-i+1)%mod;
+                ic_c = (long)(ic_c + mid-i+1)%mod;
                 ans[k] = A[j];
                 j++;
             }
