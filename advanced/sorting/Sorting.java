815d9db63780284b8bc6469aed78e66a72fab712
Ronit Nandwani <ronitnandwani@gmail.com>
2024-01-21 08:23:35 +0530

    Modified: sorting added problem for b closest points to origin

--- advanced/sorting/Sorting.java
+++ advanced/sorting/Sorting.java
@@ -56,6 +56,65 @@ class SortByFactors implements Comparator<Integer> {
 
 public class Sorting {
 
+    // B Closest Points To Origin
+    // We have a list A of points (x, y) on the plane. Find the B closest points to the origin (0, 0).
+    //Here, the distance between two points on a plane is the Euclidean distance.
+    //You may return the answer in any order. The answer is guaranteed to be unique (except for the order that it is in.)
+    //NOTE: Euclidean distance between two points P1(x1, y1) and P2(x2, y2) is sqrt( (x1-x2)2 + (y1-y2)2 ).
+    //Problem Constraints
+    //1 <= B <= length of the list A <= 105
+    //-105 <= A[i][0] <= 105
+    //-105 <= A[i][1] <= 105
+    //Input Format
+    //The argument given is list A and an integer B.
+    //Output Format
+    //Return the B closest points to the origin (0, 0) in any order.
+    //Example Input
+    //Input 1:
+    // A = [
+    //       [1, 3],
+    //       [-2, 2]
+    //     ]
+    // B = 1
+    //Input 2:
+    // A = [
+    //       [1, -1],
+    //       [2, -1]
+    //     ]
+    // B = 1
+    //Example Output
+    //Output 1:
+    // [ [-2, 2] ]
+    //Output 2:
+    // [ [1, -1] ]
+    //Example Explanation
+    //Explanation 1:
+    // The Euclidean distance will be sqrt(10) for point [1,3] and sqrt(8) for point [-2,2].
+    // So one closest point will be [-2,2].
+    //Explanation 2:
+    // The Euclidean distance will be sqrt(2) for point [1,-1] and sqrt(5) for point [2,-1].
+    // So one closest point will be [1,-1].
+    public int[][] bClosestPointsToOrigin(int[][] A, int B) {
+        // Using built-in sort function Arrays.sort
+        Arrays.sort(A, new Comparator<int[]>() {
+            @Override
+            // Compare values according to columns
+            public int compare(final int[] f,
+                               final int[] s)
+            {
+
+                // To sort in descending order revert
+                // the '>' Operator
+                return (f[0]*f[0] + f[1]*f[1]) - (s[0]*s[0] + s[1]*s[1]);
+            }
+        }); // End of function call sort().
+        int[][] ans = new int[B][2];
+        for(int i = 0;i<B;i++) {
+            ans[i] = A[i];
+        }
+        return ans;
+    }
+
     // Form largest number
     // Given an array A of non-negative integers, arrange them such that they form the largest number.
     //Note: The result may be very large, so you need to return a string instead of an integer.
