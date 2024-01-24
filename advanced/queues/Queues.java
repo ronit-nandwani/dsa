148305af5f58a7ca51b45b749519cb1d1eb7efb6
Ronit Nandwani <ronitnandwani@gmail.com>
2024-01-24 09:36:29 +0530

    Modified: queues added problem to find perfect number

--- advanced/queues/Queues.java
+++ advanced/queues/Queues.java
@@ -1,6 +1,8 @@
 package advanced.queues;
 
 
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Stack;
 
 
@@ -112,4 +114,102 @@ class UserQueue {
     }
 }
 public class Queues {
+
+    // Perfect Number
+    // Given an integer A, you have to find the Ath Perfect Number.
+    //A Perfect Number has the following properties:
+    //It comprises only 1 and 2.
+    //The number of digits in a Perfect number is even.
+    //It is a palindrome number.
+    //For example, 11, 22, 112211 are Perfect numbers, where 123, 121, 782, 1 are not.
+    //Problem Constraints
+    //1 <= A <= 100000
+    //Input Format
+    //The only argument given is an integer A.
+    //Output Format
+    //Return a string that denotes the Ath Perfect Number.
+    //Example Input
+    //Input 1:
+    // A = 2
+    //Input 2:
+    // A = 3
+    //Example Output
+    //Output 1:
+    // 22
+    //Output 2:
+    // 1111
+    //Example Explanation
+    //Explanation 1:
+    //First four perfect numbers are:
+    //1. 11
+    //2. 22
+    //3. 1111
+    //4. 1221
+    //Return the 2nd Perfect number.
+    //Explanation 2:
+    //First four perfect numbers are:
+    //1. 11
+    //2. 22
+    //3. 1111
+    //4. 1221
+    //Return the 3rd Perfect number.
+    public String perfectNumber(int A) {
+        if(A <= 2) {
+            return A+""+A;
+        }
+
+        Queue<Long> q = new LinkedList<>();
+        q.add((long)1);
+        q.add((long)2);
+        int i = 3;
+        while(i <= A) {
+            long x = q.remove();
+            long a=x*10 + 1;
+            long b=x*10 + 2;
+
+            if((long)i == A) {
+                String s = new StringBuilder(""+a).reverse().toString();
+                return ""+a+""+s;
+            }
+            if((long)(i+1) == A) {
+                String s = new StringBuffer(""+b).reverse().toString();
+                return ""+b+""+s;
+            }
+            q.add(a);q.add(b);
+            i += 2;
+        }
+        return "";
+
+        /*
+ Optimized Code
+        Queue < String > q = new LinkedList < String > ();
+        if (A == 1)
+            return "11";
+        else if (A == 2)
+            return "22";
+        q.add("1");
+        q.add("2");
+        int cur = 2;
+        String ans = new String();
+        while (q.size() < A) {
+            StringBuilder sb = new StringBuilder(q.peek());
+            q.remove();
+            sb.append("1");
+            q.add(sb.toString());
+            cur++;
+            if (cur == A)
+                ans = sb.toString();
+            sb.deleteCharAt(sb.length() - 1);
+            sb.append("2");
+            cur++;
+            if (cur == A)
+                ans = sb.toString();
+            q.add(sb.toString());
+        }
+        // ans has the first half of our final answer
+        StringBuilder sb = new StringBuilder(ans);
+        return ans + sb.reverse().toString();
+*/
+
+    }
 }
