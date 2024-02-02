17728bcb81646b6a9f8b50c5c5c7b932f8da90ce
Ronit Nandwani <ronitnandwani@gmail.com>
2024-02-02 07:36:00 +0100

    Modified: Trees1 problem added for post order traversal

--- advanced/trees/Trees1.java
+++ advanced/trees/Trees1.java
@@ -13,6 +13,51 @@ class TreeNode {
 }
 public class Trees1 {
 
+    // Post Order Traversal
+    // Given a binary tree, return the Postorder traversal of its nodes values.
+    //Problem Constraints
+    //1 <= number of nodes <= 105
+    //Input Format
+    //First and only argument is root node of the binary tree, A.
+    //Output Format
+    //Return an integer array denoting the Postorder traversal of the given binary tree.
+    //Example Input
+    //Input 1:
+    //
+    //   1
+    //    \
+    //     2
+    //    /
+    //   3
+    //Input 2:
+    //
+    //   1
+    //  / \
+    // 6   2
+    //    /
+    //   3
+    //Example Output
+    //Output 1:
+    // [3, 2, 1]
+    //Output 2:
+    // [6, 3, 2, 1]
+    //Example Explanation
+    //Explanation 1:
+    // The Preoder Traversal of the given tree is [3, 2, 1].
+    //Explanation 2:
+    // The Preoder Traversal of the given tree is [6, 3, 2, 1].
+    ArrayList<Integer> postArr = new ArrayList<Integer>();
+    public void postOrder(TreeNode A) {
+        if(A == null) return;
+        postOrder(A.left);
+        postOrder(A.right);
+        postArr.add(A.val);
+    }
+    public ArrayList<Integer> postorderTraversal(TreeNode A) {
+        postOrder(A);
+        return postArr;
+    }
+
     // Binary tree from inorder and postorder
     // Given the inorder and postorder traversal of a tree, construct the binary tree.
     // NOTE: You may assume that duplicates do not exist in the tree.
