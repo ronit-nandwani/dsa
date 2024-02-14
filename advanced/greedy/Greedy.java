f093ec3d5900dba25f17e7ab1f283684ca4d4454
Ronit Nandwani <ronitnandwani@gmail.com>
2024-02-14 21:32:25 +0530

    Modified: greedy problem added free cars

--- advanced/greedy/Greedy.java
+++ advanced/greedy/Greedy.java
@@ -1,6 +1,92 @@
 package advanced.greedy;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+class Pair {
+    int time,profit;
+    Pair(int x,int y) {
+        time=x;
+        profit=y;
+    }
+}
 public class Greedy {
+    
+    // Free Cars
+    // Given two arrays, A and B of size N. A[i] represents the time by which you can buy the ith car without paying any money.
+    //B[i] represents the profit you can earn by buying the ith car. It takes 1 minute to buy a car, so you can only buy the ith car when the current time <= A[i] - 1.
+    //Your task is to find the maximum profit one can earn by buying cars considering that you can only buy one car at a time.
+    //NOTE:
+    //You can start buying from time = 0.
+    //Return your answer modulo 109 + 7.
+    //Problem Constraints
+    //1 <= N <= 105
+    //1 <= A[i] <= 109
+    //0 <= B[i] <= 109
+    //Input Format
+    //The first argument is an integer array A represents the deadline for buying the cars.
+    //The second argument is an integer array B represents the profit obtained after buying the cars.
+    //Output Format
+    //Return an integer denoting the maximum profit you can earn.
+    //Example Input
+    //Input 1:
+    // A = [1, 3, 2, 3, 3]
+    // B = [5, 6, 1, 3, 9]
+    //Input 2:
+    // A = [3, 8, 7, 5]
+    // B = [3, 1, 7, 19]
+    //Example Output
+    //Output 1:
+    // 20
+    //Output 2:
+    // 30
+    //Example Explanation
+    //Explanation 1:
+    // At time 0, buy car with profit 5.
+    // At time 1, buy car with profit 6.
+    // At time 2, buy car with profit 9.
+    // At time = 3 or after , you can't buy any car, as there is no car with deadline >= 4.
+    // So, total profit that one can earn is 20.
+    //Explanation 2:
+    // At time 0, buy car with profit 3.
+    // At time 1, buy car with profit 1.
+    // At time 2, buy car with profit 7.
+    // At time 3, buy car with profit 19.
+    // We are able to buy all cars within their deadline. So, total profit that one can earn is 30.
+    public int freeCars(int[] A, int[] B) {
+        ArrayList<Pair> cars = new ArrayList<>();
+        int n = A.length;
+        for(int i = 0;i < n; i++) {
+            Pair p = new Pair(A[i],B[i]);
+            cars.add(p);
+        }
+        Collections.sort(cars,new Comparator<Pair>(){
+            public int compare(Pair a, Pair b){
+                return a.time-b.time;
+            }
+        });
+        PriorityQueue <Integer> pq = new PriorityQueue<>();
+        int t=0, ans=0;
+        for(int i=0;i<n;i++) {
+            Pair p = cars.get(i);
+            if(t<p.time) {
+                pq.add(p.profit);
+                t++;
+            } else {
+                if(!pq.isEmpty() && p.profit > pq.peek()) {
+                    pq.remove();
+                    pq.add(p.profit);
+                }
+            }
+        }
+        while(!pq.isEmpty()) {
+            ans = (ans + pq.poll())%1000000007;
+        }
+        return ans;
+    }
+
     // Distribute Candy
     // N children are standing in a line. Each child is assigned a rating value.
     //You are giving candies to these children subjected to the following requirements:
