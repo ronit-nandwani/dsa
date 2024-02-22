23124a3c5d4786fcf1b4d1d30e777432613f5825
Ronit Nandwani <ronitnandwani@gmail.com>
2024-02-22 07:36:00 +0100

    Modified: heaps 1 added problem to merge k sorted lists

--- advanced/heaps/Heaps1.java
+++ advanced/heaps/Heaps1.java
@@ -1,6 +1,120 @@
 package advanced.heaps;
 import java.util.*;
+
+  class ListNode {
+      public int val;
+      public ListNode next;
+      ListNode(int x) { val = x; next = null; }
+ }
 public class Heaps1 {
+      // Merge K Sorted Lists
+    // Given a list containing head pointers of N sorted linked lists.
+    //Merge these given sorted linked lists and return them as one sorted list.
+    //Problem Constraints
+    //1 <= total number of elements in given linked lists <= 100000
+    //Input Format
+    //The first and only argument is a list containing N head pointers.
+    //Output Format
+    //Return a pointer to the head of the sorted linked list after merging all the given linked lists.
+    //Example Input
+    //Input 1:
+    // 1 -> 10 -> 20
+    // 4 -> 11 -> 13
+    // 3 -> 8 -> 9
+    //Input 2:
+    // 10 -> 12
+    // 13
+    // 5 -> 6
+    //Example Output
+    //Output 1:
+    // 1 -> 3 -> 4 -> 8 -> 9 -> 10 -> 11 -> 13 -> 20
+    //Output 2:
+    // 5 -> 6 -> 10 -> 12 ->13
+    //Example Explanation
+    //Explanation 1:
+    // The resulting sorted Linked List formed after merging is 1 -> 3 -> 4 -> 8 -> 9 -> 10 -> 11 -> 13 -> 20.
+    //Explanation 2:
+    // The resulting sorted Linked List formed after merging is 5 -> 6 -> 10 -> 12 ->13.
+    public static ListNode mergeKLists(ArrayList<ListNode> a) {
+        int n = a.size();
+        PriorityQueue <ListNode> pq = new PriorityQueue<>(new Comparator<ListNode>(){
+            public int compare(ListNode s1, ListNode s2) {
+                if (s1.val < s2.val)
+                    return -1;
+                else if (s1.val > s2.val)
+                    return 1;
+                return 0;
+            }
+        });
+        for(int i=0;i<n;i++) {
+            pq.add(a.get(i));
+        }
+        ListNode head = null;
+        ListNode temp = head;
+        while(!pq.isEmpty()) {
+            if(head == null) {
+                head = pq.peek();
+                temp = pq.remove();
+            } else {
+                temp.next = pq.peek();
+                pq.remove();
+                temp = temp.next;
+            }
+            if(temp.next != null) {
+                pq.add(temp.next);
+            }
+        }
+        return head;
+    }
+    // Solution by them
+    // public class Solution {
+    
+    //     public ListNode mergeKLists(ArrayList<ListNode> A) {
+            
+    //         TreeMap<Integer, ArrayList<ListNode>> map = new TreeMap<>();
+    //         ListNode node;
+    //         ArrayList<ListNode> list;
+    //         int val;
+            
+    //         for (ListNode head : A) {
+    //             node = head;
+    //             while (node != null) {
+    //                 val = node.val;
+                    
+    //                 if (map.containsKey(val)) {
+    //                     list = map.get(val);
+    //                     list.add(node);
+    //                 } else {
+    //                     list = new ArrayList<>();
+    //                     list.add(node);
+    //                     map.put(val, list);
+    //                 }
+                    
+    //                 node = node.next;
+    //             }
+    //         }
+            
+    //         ListNode head = null;
+    //         node = null;
+            
+    //         for (Map.Entry<Integer, ArrayList<ListNode>> entry : map.entrySet()) {
+                
+    //             list = entry.getValue();
+                
+    //             for (ListNode n : list) {
+    //                 if (head == null)
+    //                     head = n;
+    //                 if (node != null)
+    //                     node.next = n;
+    //                 node = n;
+    //             }
+                
+    //         }
+            
+    //         return head;
+    //     }
+    // }
+
     // Connect Ropes
     // You are given an array A of integers that represent the lengths of ropes.
     //You need to connect these ropes into one rope. The cost of joining two ropes equals the sum of their lengths.
@@ -55,5 +169,20 @@ public class Heaps1 {
             ans += sum;
         }
         return ans;
+    }
+
+    public static void main(String[] args) {
+        // Input
+        // 2
+        //2 1 2
+        //3 1 2 3
+        ArrayList<ListNode> arr = new ArrayList<>();
+        ListNode l1 = new ListNode(2);arr.add(l1);
+        ListNode l2 = new ListNode(3);l1.next = l2;
+        ListNode l3 = new ListNode(4);arr.add(l3);
+        ListNode l4 = new ListNode(5);l3.next = l4;
+        ListNode l5 = new ListNode(6);l4.next = l5;
+
+        System.out.println(mergeKLists(arr));
     }
 }
