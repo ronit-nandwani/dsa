083c5658af89d7d14f3ebea181e6dd34927ba6c3
Ronit Nandwani <ronitnandwani@gmail.com>
2024-03-02 07:36:00 +0100

    Modified: 2d arrays added problem to find minor diagonal sum

--- advanced/arrays/Arrays2D.java
+++ advanced/arrays/Arrays2D.java
@@ -1,6 +1,57 @@
 package advanced.arrays;
 
 public class Arrays2D {
+    // Minor Diagonal Sum
+
+    // You are given a N X N integer matrix. You have to find the sum of all the minor diagonal elements of A.
+    // Minor diagonal of a M X M matrix A is a collection of elements A[i, j] such that i + j = M + 1 (where i, j are 1-based).
+    // Problem Constraints
+    // 1 <= N <= 103
+    // -1000 <= A[i][j] <= 1000
+    // Input Format
+    // First and only argument is a 2D integer matrix A.
+    // Output Format
+    // Return an integer denoting the sum of minor diagonal elements.
+    // Example Input
+    // Input 1:
+    // A = [[1, -2, -3],
+    //     [-4, 5, -6],
+    //     [-7, -8, 9]]
+    // Input 2:
+    // A = [[3, 2],
+    //     [2, 3]]
+    // Example Output
+    // Output 1:
+    // -5 
+    // Output 2:
+    // 4 
+    // Example Explanation
+    // Explanation 1:
+    // A[1][3] + A[2][2] + A[3][1] = (-3) + 5 + (-7) = -5
+    // Explanation 2:
+    // A[1][2] + A[2][1] = 2 + 2 = 4
+    public int minorDiagonalSum(final int[][] A) {
+        int n = A.length, result = 0;
+        for(int i = 0; i < n; i++){
+            result += A[n-i-1][i];
+        }
+        return result;
+    }
+    // Solution by team
+    // public class Solution {
+    //     // DO NOT MODIFY THE ARGUMENTS WITH "final" PREFIX. IT IS READ ONLY
+    //     public int solve(final int[][] A) {
+    //         int N = A.length;
+        
+    //         int sum = 0;
+    //         for (int i = 0; i < N;i++){
+    //             sum += A[i][N - 1 - i];
+    //         }
+        
+    //         return sum;
+    //     }
+    // }
+
     // You are given a 2D integer matrix A, make all the elements in a row or column zero if the A[i][j] = 0. Specifically, make entire ith row and jth column zero.
     public int[][] rowToColumnZero(int[][] A) {
         int n = A.length;
