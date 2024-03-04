bda4ffed3ace21857987774339426db007c6c767
Ronit Nandwani <ronitnandwani@gmail.com>
2024-03-04 07:36:00 +0100

    Modified: strings added problem to find longest common prefix

--- intermediate/strings/Strings.java
+++ intermediate/strings/Strings.java
@@ -1,8 +1,79 @@
 package intermediate.strings;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.TreeSet;
 
 public class Strings {
+
+    // Longest Common Prefix
+
+    // Given the array of strings A, you need to find the longest string S, which is the prefix of ALL the strings in the array.
+    // The longest common prefix for a pair of strings S1 and S2 is the longest string S which is the prefix of both S1 and S2.
+    // Example: the longest common prefix of "abcdefgh" and "abcefgh" is "abc".
+    // Problem Constraints
+    // 0 <= sum of length of all strings <= 1000000
+    // Input Format
+    // The only argument given is an array of strings A.
+    // Output Format
+    // Return the longest common prefix of all strings in A.
+    // Example Input
+    // Input 1:
+    // A = ["abcdefgh", "aefghijk", "abcefgh"]
+    // Input 2:
+    // A = ["abab", "ab", "abcd"];
+    // Example Output
+    // Output 1:
+    // "a"
+    // Output 2:
+    // "ab"
+    // Example Explanation
+    // Explanation 1:
+    // Longest common prefix of all the strings is "a".
+    // Explanation 2:
+    // Longest common prefix of all the strings is "ab".
+    public String longestCommonPrefix(String[] A) {
+         int n=A.length;
+        Arrays.sort(A);
+        String ans="";
+        if(n==1) return A[0];
+        if(n==0) return "";
+        for(int i=0;i < A[0].length(); i++) {
+            if(A[0].charAt(i)== A[n-1].charAt(i) ) {
+                ans=ans + A[0].charAt(i);
+            }
+            else{
+                break;
+            }
+        }
+        return ans;
+    }
+    // Solution by team
+    // public class Solution {
+    // public String longestCommonPrefix(ArrayList < String > A) {
+    //     if (A.size() == 0)
+    //         return "";
+    //     String str;
+    //     String res = "";
+    //     int min = Integer.MAX_VALUE;
+    //     for (int i = 0; i < A.size(); i++) {
+    //         min = Math.min(min, A.get(i).length());
+    //     }
+    //     for (int i = 0; i < min; i++) {
+    //         char c = A.get(0).charAt(i);
+    //         // check if c character is same in every string or not    
+    //         for (int j = 1; j < A.size(); j++) {
+
+    //             if (c != A.get(j).charAt(i))
+    //                 return res;
+    //         }
+    //         res += c;
+    //     }
+    //     return res;
+    // }
+    // }
+
+
     // Amazing Sub Arrays
 
     // You are given a string S, and you have to find all the amazing substrings of S.
