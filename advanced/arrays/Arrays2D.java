a97447f68c6a8232018a6a376675974f9fd40075
Ronit Nandwani <ronitnandwani@gmail.com>
2024-03-05 12:53:11 +0530

    Modified: arrays 2d to add problem to find row sum

--- advanced/arrays/Arrays2D.java
+++ advanced/arrays/Arrays2D.java
@@ -1,6 +1,54 @@
 package advanced.arrays;
 
 public class Arrays2D {
+    // Row Sum
+
+    // 1 <= A.size() <= 103
+    // 1 <= A[i].size() <= 103
+    // 1 <= A[i][j] <= 103
+    // Input Format
+    // First argument A is a 2D array of integers.(2D matrix).
+    // Output Format
+    // Return an array containing row-wise sums of original matrix.
+    // Example Input
+    // Input 1:
+    // [1,2,3,4]
+    // [5,6,7,8]
+    // [9,2,3,4]
+    // Example Output
+    // Output 1:
+    // [10,26,18]
+    // Example Explanation
+    // Explanation 1
+    // Row 1 = 1+2+3+4 = 10
+    // Row 2 = 5+6+7+8 = 26
+    // Row 3 = 9+2+3+4 = 18
+    public int[] solve(int[][] A) {
+        int[] result = new int[A.length];
+        for(int i = 0; i < A.length; i++){
+        for(int j = 0; j < A[0].length; j++){
+        result[i] += A[i][j];
+        }
+        }
+        return result;
+    }
+    // Solution by team
+    // public class Solution {
+    //     public int[] solve(int[][] A) {
+    //         int n = A.length, m = A[0].length;
+    //         int ans[] = new int[n];
+    //         for(int i = 0; i < n; i++){
+    //             int temp = 0;
+    //             // Finding sum of elements of ith row
+    //             for(int j = 0; j < m; j++){
+    //                 temp += A[i][j];
+    //             }
+    //             ans[i] = temp;
+    //         }
+    //         return ans;
+    //     }
+    // }
+
     // Minor Diagonal Sum
 
     // You are given a N X N integer matrix. You have to find the sum of all the minor diagonal elements of A.
