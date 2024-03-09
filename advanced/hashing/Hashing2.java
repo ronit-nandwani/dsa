2b869718e556af58ad6e356dcca6c999bb76ef75
Ronit Nandwani <ronitnandwani@gmail.com>
2024-03-09 17:45:20 +0530

    Modified: Hashing 2 added problem to find Count pair sum

--- advanced/hashing/Hashing2.java
+++ advanced/hashing/Hashing2.java
@@ -2,8 +2,71 @@ package advanced.hashing;
 
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 
 public class Hashing2 {
+    // Count Pair Sum
+
+    // You are given an array A of N integers and an integer B. Count the number of pairs (i,j) such that A[i] + A[j] = B and i ≠ j.
+    // Since the answer can be very large, return the remainder after dividing the count with 109+7.
+    // Note - The pair (i,j) is same as the pair (j,i) and we need to count it only once.
+    // Problem Constraints
+    // 1 <= N <= 105
+    // 1 <= A[i] <= 109
+    // 1 <= B <= 109
+    // Input Format
+    // First argument A is an array of integers and second argument B is an integer.
+    // Output Format
+    // Return an integer.
+    // Example Input
+    // Input 1:
+    // A = [3, 5, 1, 2]
+    // B = 8
+    // Input 2:
+    // A = [1, 2, 1, 2]
+    // B = 3
+    // Example Output
+    // Output 1:
+    // 1
+    // Output 2:
+    // 4
+    // Example Explanation
+    // Example 1:
+    // The only pair is (1, 2) which gives sum 8
+    // Example 2:
+    // The pair which gives sum as 3 are (1, 2), (1, 4), (2, 3) and (3, 4).
+    public int countPairSum(int[] A, int B) {
+        int n = A.length;
+        HashMap<Integer,Integer> hm = new HashMap();
+        int ans = 0;
+        for(int i=0;i<n;i++) {
+            if(hm.containsKey(B-A[i])) {
+                ans = ans + hm.get(B-A[i]);
+            }
+            if(hm.containsKey(A[i])) {
+                hm.put(A[i], hm.get(A[i])+1);
+            } else {
+                hm.put(A[i],1);
+            }
+        }
+        return ans%1000000007;
+    }
+    // Solution by team
+    // public class Solution {
+    //     public int solve(int[] A, int B) {
+    //         Map<Integer, Integer> freq = new HashMap<Integer, Integer>();
+    //         final int mod = (int)(1e9 + 7);
+    //         long ans = 0;
+    //         for(int i = 0 ; i < A.length ; i++){
+    //             if(freq.containsKey(B - A[i])){
+    //                 ans = (ans + freq.get(B - A[i])) % mod;
+    //             }
+    //             freq.put(A[i], freq.getOrDefault(A[i], 0) + 1);
+    //         }
+    //         return (int)(ans);
+    //     }
+    // }
+
     // You are given an array A of N integers and an integer B.
     // Count the number of pairs (i,j) such that A[i] - A[j] = B and i ≠ j.
     // Since the answer can be very large, return the remainder after dividing the count with 109+7.
