07cab9fada3d451ec4bc0d25769234c9f0b82e84
Ronit Nandwani <ronitnandwani@gmail.com>
2024-03-11 11:09:55 +0530

    Modified: strings added problem to find is alpha numeric

--- intermediate/strings/Strings.java
+++ intermediate/strings/Strings.java
@@ -5,6 +5,55 @@ import java.util.Arrays;
 import java.util.TreeSet;
 
 public class Strings {
+    // Is alphanumeric Isalnum()
+
+    // You are given a function isalpha() consisting of a character array A.
+    // Return 1 if all the characters of a character array are alphanumeric (a-z, A-Z, and 0-9) else, return 0.
+    // Problem Constraints
+    // 1 <= |A| <= 105
+    // Input Format
+    // Only argument is a character array A.
+    // Output Format
+    // Return 1 if all the characters of the character array are alphanumeric (a-z, A-Z and 0-9), else return 0.
+    // Example Input
+    // Input 1:
+    // A = ['S', 'c', 'a', 'l', 'e', 'r', 'A', 'c', 'a', 'd', 'e', 'm', 'y', '2', '0', '2', '0']
+    // Input 2:
+    // A = ['S', 'c', 'a', 'l', 'e', 'r', '#', '2', '0', '2', '0']
+    // Example Output
+    // Output 1:
+    // 1
+    // Output 2:
+    // 0
+    // Example Explanation
+    // Explanation 1:
+    // All the characters are alphanumeric.
+    // Explanation 2:
+    // All the characters are NOT alphabets i.e ('#').
+    public int solve(char[] A) {
+        int n = A.length;
+       for (int i = 0; i < n; i++) {
+           if ((A[i] < 'a' || A[i] > 'z') && (A[i] < 'A' || A[i] > 'Z') && (A[i] < '0' || A[i] > '9')) {
+               return 0;
+           }
+       }
+       return 1;
+   }
+   // Solution by team
+//    public class Solution {
+//     public int solve(char[] A) {
+//         int n = A.length;
+//         for(int i = 0; i < n; i++) {
+//             char c = A[i];
+//             if((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z') || (c >= '0' && c <= '9')) {
+//                 continue;
+//             }
+//             return 0;
+//         }
+//         return 1;
+//     }
+//     }
+   
 
     // Longest Common Prefix
 
