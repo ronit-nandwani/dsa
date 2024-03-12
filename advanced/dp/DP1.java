e1bb2f3230dcc86ba27f7eb39389921619e235be
Ronit Nandwani <ronitnandwani@gmail.com>
2024-03-12 19:22:50 +0530

    Modified: dp1 added problem for finding maximum sum without adjacent elements

--- advanced/dp/DP1.java
+++ advanced/dp/DP1.java
@@ -1,8 +1,92 @@
 package advanced.dp;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class DP1 {
+    // Max Sum without Adjacent Elements
+
+    // Given a 2 x N grid of integers, A, your task is to choose numbers from the grid such that sum of these numbers is maximized. 
+    // However, you cannot choose two numbers that are adjacent horizontally, vertically, or diagonally. 
+    // Return the maximum possible sum.
+    // Note: You are allowed to choose more than 2 numbers from the grid.
+    // Problem Constraints
+    // 1 <= N <= 20000
+    // 1 <= A[i] <= 2000
+    // Input Format
+    // The first and the only argument of input contains a 2d matrix, A.
+    // Output Format
+    // Return an integer, representing the maximum possible sum.
+    // Example Input
+    // Input 1:
+    // A = [   
+    //         [1]
+    //         [2]    
+    //     ]
+    // Input 2:
+    // A = [   
+    //         [1, 2, 3, 4]
+    //         [2, 3, 4, 5]    
+    //     ]
+    // Example Output
+    // Output 1:
+    // 2
+    // Output 2:
+    // 8
+    // Example Explanation
+    // Explanation 1:
+    // We will choose 2 (From 2nd row 1st column).
+    // Explanation 2:
+    // We will choose 3 (From 2nd row 2nd column) and 5 (From 2nd row 4th column).
+    public int adjacent(int[][] A) {
+        int n = A[0].length;
+        int[] merged = new int[n];
+        for(int i=0;i<n;i++) {
+            merged[i] = Math.max(A[0][i],A[1][i]);
+        }
+
+        int[] res = new int[n];
+        res[0] = merged[0];
+        if(n>1) res[1] = Math.max(merged[0],merged[1]);
+        
+        for(int i=2;i<n;i++) {
+            res[i] = Math.max(res[i-1],merged[i]+res[i-2]);
+        }
+        return res[n-1];
+    }
+    // Solution by team
+    //     public class Solution {
+    // public int adjacent(ArrayList < ArrayList < Integer >> A) {
+
+    //     int n;
+    //     int dp[];
+    //     ArrayList < Integer > first, second;
+    //     first = A.get(0);
+    //     second = A.get(1);
+
+    //     n = A.get(0).size();
+
+    //     if (n == 0)
+    //     return 0;
+
+    //     dp = new int[n];
+    //     dp[0] = Math.max(first.get(0), second.get(0));
+
+    //     if (n < 2)
+    //     return dp[0];
+
+    //     dp[1] = Math.max(first.get(1), second.get(1));
+    //     dp[1] = Math.max(dp[0], dp[1]);
+
+    //     for (int i = 2; i < n; i++) {
+    //     dp[i] = Math.max(first.get(i), second.get(i));
+    //     dp[i] += dp[i - 2];
+    //     dp[i] = Math.max(dp[i], dp[i - 1]);
+    //     }
+
+    //     return dp[n - 1];
+    // }
+    // }
 
     // Minimum No. of squares
 
