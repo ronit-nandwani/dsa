cc8e31e1247f564d3c4aa79d2ad15154ab69ac48
Ronit Nandwani <ronitnandwani@gmail.com>
2024-03-13 07:36:00 +0100

    Modified: stacks 1 to add double character trouble problem

--- advanced/stacks/Stacks1.java
+++ advanced/stacks/Stacks1.java
@@ -3,7 +3,79 @@ package advanced.stacks;
 import java.util.Stack;
 
 public class Stacks1 {
-    
+    // Double Character Trouble
+
+    // You have a string, denoted as A.
+    // To transform the string, you should perform the following operation repeatedly:
+    // Identify the first occurrence of consecutive identical pairs of characters within the string.
+    // Remove this pair of identical characters from the string.
+    // Repeat steps 1 and 2 until there are no more consecutive identical pairs of characters.
+    // The final result will be the transformed string.
+    // Problem Constraints
+    // 1 <= |A| <= 100000
+    // Input Format
+    // First and only argument is string A.
+    // Output Format
+    // Return the final string.
+    // Example Input
+    // Input 1:
+    // A = "abccbc"
+    // Input 2:
+    // A = "ab"
+    // Example Output
+    // Output 1:
+    // "ac"
+    // Output 2:
+    // "ab"
+    // Example Explanation
+    // Explanation 1:
+    // The Given string is "abccbc".
+    // Remove the first occurrence of consecutive identical pairs of characters "cc".
+    // After removing the string will be "abbc".
+    // Again Removing the first occurrence of consecutive identical pairs of characters "bb".
+    // After remvoing, the string will be "ac".
+    // Now, there is no consecutive identical pairs of characters.
+    // Therefore the string after this operation will be "ac".
+    // Explanation 2:
+
+    // No removals are to be done.
+    public String doubleCharacterTrouble(String A) {
+        Stack<Character> st = new Stack();
+        for(int i=0; i<A.length();i++) {
+            if(!st.isEmpty() && st.peek() == A.charAt(i)) {
+                st.pop();
+            } else {
+                st.push(A.charAt(i));
+            }
+        }
+        String s = "";
+        while(st.size()>0) {
+            s += st.pop();
+        }
+        String b = new StringBuffer(s).reverse().toString();
+        return b;
+    }
+    // Solution by team
+    // public String solve(String A) {
+    //     // we maintain a stack for the characters of the string
+    //     Stack < Character > st = new Stack < Character > ();
+    //     for (int i = 0; i < A.length(); i++) {
+    //         // if the current character is equal to the top of the stack then they form 
+    //         // a pair of consecutive similar characters therefore we pop from the stack,
+    //         // else we push the charcter in the stack
+    //         if (!st.empty() && st.peek() == A.charAt(i)) {
+    //             st.pop();
+    //         } else st.push(A.charAt(i));
+    //     }
+    //     StringBuilder sb = new StringBuilder();
+    //     while (!st.empty()) {
+    //         sb.append(st.peek());
+    //         st.pop();
+    //     }
+    //     sb.reverse();
+    //     return sb.toString();
+    // }
+
     // Passing Game
     // There is a football event going on in your city. In this event, you are given A passes and players having ids between 1 and 106.
     //Initially, some player with a given id had the ball in his possession. You have to make a program to display the id of the player who possessed the ball after exactly A passes.
