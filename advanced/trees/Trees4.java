cab9302bce02ad8f5be82b68b4015fc422a91781
Ronit Nandwani <ronitnandwani@gmail.com>
2024-03-17 07:36:00 +0100

    Modified: trees 4 to add problem to recover BST

--- advanced/trees/Trees4.java
+++ advanced/trees/Trees4.java
@@ -3,6 +3,125 @@ package advanced.trees;
 import java.util.ArrayList;
 
 public class Trees4 {
+    // Recover BST
+
+    // Two elements of a Binary Search Tree (BST), represented by root A are swapped by mistake. Tell us the 2 values, when swapped, will restore the Binary Search Tree (BST).
+    // A solution using O(n) space is pretty straightforward. Could you devise a constant space solution?
+    // Note: The 2 values must be returned in ascending order
+    // Problem Constraints
+    // 1 <= size of tree <= 100000
+    // Input Format
+    // First and only argument is the head of the tree,A
+    // Output Format
+    // Return the 2 elements which need to be swapped.
+    // Example Input
+    // Input 1:
+    //         1 
+    //         / \ 
+    //     2   3
+    // Input 2:
+    //         2
+    //         / \
+    //     3   1
+    // Example Output
+    // Output 1:
+    // [2, 1]
+    // Output 2:
+    // [3, 1]
+    // Example Explanation
+    // Explanation 1:
+    // Swapping 1 and 2 will change the BST to be 
+    //         2
+    //         / \
+    //     1   3
+    // which is a valid BST 
+    // Explanation 2:
+    // Swapping 1 and 3 will change the BST to be 
+    //         2
+    //         / \
+    //     1   3
+    // which is a valid BST
+
+    public ArrayList<Integer> recoverTree(TreeNode A) {
+        ArrayList<Integer> inArr = new ArrayList<Integer>();
+        TreeNode curr = A;
+        TreeNode first = null;
+        TreeNode prev = null;
+        TreeNode second = null;
+        while(curr != null) {
+            if(curr.left == null) {
+                // Process curr node
+                if (prev != null && prev.val > curr.val) {
+                    if (first == null) {
+                        first = prev;
+                    }
+                    second = curr;
+                }
+                prev = curr;
+                curr = curr.right;
+            } else {
+                TreeNode pre = curr.left;
+                while(pre.right != null && pre.right != curr) {
+                    pre = pre.right;
+                }
+                if(pre.right == null) {
+                    pre.right = curr;
+                    curr = curr.left;
+                } else {
+                    pre.right = null;
+                    // Process curr node
+                if (prev != null && prev.val > curr.val) {
+                    if (first == null) {
+                        first = prev;
+                    }
+                    second = curr;
+                }
+                prev = curr;
+                    curr = curr.right;
+                }
+            }
+        }
+        if(second != null) {
+            inArr.add(second.val);
+        }
+        if (first != null) inArr.add(first.val);
+        return inArr;
+    }
+    // Solution by team
+    // public class Solution {
+    //     private TreeNode left;
+    //     private TreeNode right;
+    //     private TreeNode prev;
+    //     public int[] recoverTree(TreeNode root) {
+    //         int[] res = new int[2];
+    //         dfs(root);
+    //         if (left == null) return res;
+    //         int tmp = left.val;
+    //         left.val = right.val;
+    //         right.val = tmp;
+    //         res[0] = left.val;
+    //         res[1] = right.val;
+    //         return res;
+    //     }
+    //     private void dfs(TreeNode node) {
+    //         if (node == null)
+    //             return;
+    //         dfs(node.left);
+    //         if (prev != null) {
+    //             if (node.val < prev.val) {
+    //                 if (left == null) {
+    //                     left = prev;
+    //                     right = node;
+    //                 } else {
+    //                     right = node;
+    //                 }
+    //             }
+    //         }
+    //         prev = node;
+    //         dfs(node.right);
+    //     }
+    // }
+
 
     // Morris Inorder Traversal - TC O(N) and SC O(1)
 
