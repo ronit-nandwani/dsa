2e2f9a5adbf7c3b1643012f1463b24e1c4371149
Ronit Nandwani <ronitnandwani@gmail.com>
2024-03-18 07:36:00 +0100

    Modified: queues to add problem to find n integers containing only 1,2,3

--- advanced/queues/Queues.java
+++ advanced/queues/Queues.java
@@ -118,6 +118,72 @@ class UserQueue {
 }
 public class Queues {
 
+    // N Integers containing only 1,2 & 3
+    //     Given an integer, A. Find and Return first positive A integers in ascending order containing only digits 1, 2, and 3.
+    // NOTE: All the A integers will fit in 32-bit integers.
+    // Problem Constraints
+    // 1 <= A <= 29500
+    // Input Format
+    // The only argument given is integer A.
+    // Output Format
+    // Return an integer array denoting the first positive A integers in ascending order containing only digits 1, 2 and 3.
+    // Example Input
+    // Input 1:
+    // A = 3
+    // Input 2:
+    // A = 7
+    // Example Output
+    // Output 1:
+    // [1, 2, 3]
+    // Output 2:
+    // [1, 2, 3, 11, 12, 13, 21]
+    // Example Explanation
+    // Explanation 1:
+    // Output denotes the first 3 integers that contains only digits 1, 2 and 3.
+    // Explanation 2:
+    // Output denotes the first 7 integers that contains only digits 1, 2 and 3.
+    public int[] nIntegersContainingOnly123(int A) {
+        Queue<Integer> q = new LinkedList<>();
+        q.add(1);q.add(2);q.add(3);
+        int count = 0;
+        int[] ans = new int[A];
+        while(!q.isEmpty()) {
+            int a = q.remove();
+            ans[count++] = a;
+            if(count == A) {
+                break;
+            }
+
+            q.add(a*10 + 1);
+            q.add(a*10 + 2);
+            q.add(a*10 + 3);
+        }
+        return ans;
+    }
+    // Solution by Team
+    // public ArrayList < Integer > nIntegersContainingOnly123(int A) {
+    //     Queue < Integer > q = new LinkedList < Integer > ();
+    //     ArrayList < Integer > ans = new ArrayList < > ();
+    //     q.add(1);
+    //     q.add(2);
+    //     q.add(3);
+    //     int cnt = 3;
+    //     while (ans.size() < A) {
+    //         int x = q.peek();
+    //         ans.add(x);
+    //         q.remove();
+    //         if(cnt >= A)continue;
+    //         // append 1, 2 and 3 to the current number
+    //         q.add(10 * x + 1);
+    //         q.add(10 * x + 2);
+    //         q.add(10 * x + 3);
+    //         cnt += 3;
+    //     }
+    //     return ans;
+    // }
+
+
+
     // Imagine you're an ice cream truck driver in a beachside town. The beach is divided into several sections, and each section has varying numbers of beachgoers wanting ice cream given by the array of integers A.
     // For simplicity, let's say the beach is divided into 8 sections. One day, you note down the number of potential customers in each section: [5, 12, 3, 4, 8, 10, 2, 7]. This means there are 5 people in the first section, 12 in the second, and so on.
     // You can only stop your truck in B consecutive sections at a time because of parking restrictions. To maximize sales, you want to park where the most customers are clustered together.
