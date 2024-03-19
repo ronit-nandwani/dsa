279ffdffb065ed52dab3125c8d211d83934c50d3
Ronit Nandwani <ronitnandwani@gmail.com>
2024-03-19 07:36:00 +0100

    Modified: stacks 2 and added problem to find max and min

--- advanced/stacks/Stacks2.java
+++ advanced/stacks/Stacks2.java
@@ -2,7 +2,261 @@ package advanced.stacks;
 
 import java.util.Stack;
 
+// Max and Min
+// Given an array of integers A.
+// The value of an array is computed as the difference between the maximum element in the array and the minimum element in the array A.
+// Calculate and return the sum of values of all possible subarrays of A modulo 109+7.
+// Problem Constraints
+// 1 <= |A| <= 100000
+// 1 <= A[i] <= 1000000
+// Input Format
+// The first and only argument given is the integer array A.
+// Output Format
+// Return the sum of values of all possible subarrays of A modulo 109+7.
+// Example Input
+// Input 1:
+// A = [1]
+// Input 2:
+// A = [4, 7, 3, 8]
+// Example Output
+// Output 1:
+// 0
+// Output 2:
+// 26
+// Example Explanation
+// Explanation 1:
+// Only 1 subarray exists. Its value is 0.
+// Explanation 2:
+// value ( [4] ) = 4 - 4 = 0
+// value ( [7] ) = 7 - 7 = 0
+// value ( [3] ) = 3 - 3 = 0
+// value ( [8] ) = 8 - 8 = 0
+// value ( [4, 7] ) = 7 - 4 = 3
+// value ( [7, 3] ) = 7 - 3 = 4
+// value ( [3, 8] ) = 8 - 3 = 5
+// value ( [4, 7, 3] ) = 7 - 3 = 4
+// value ( [7, 3, 8] ) = 8 - 3 = 5
+// value ( [4, 7, 3, 8] ) = 8 - 3 = 5
+// sum of values % 10^9+7 = 26
+class Solution {
+    public int[] nearestGreaterLeft(int[] A) {
+        Stack<Integer> st = new Stack();
+        int n = A.length;
+        int[] ans = new int[n];
+
+        for(int i=0;i<n;i++) {
+            while(!st.isEmpty() && A[st.peek()] <= A[i]) {
+                st.pop();
+            }
+            if(st.isEmpty()) {
+                ans[i] = -1;
+            } else {
+                ans[i] = st.peek();
+            }
+            st.push(i);
+        }
+        return ans;
+    }
+    public int[] nearestGreaterRight(int[] A) {
+        Stack<Integer> st = new Stack();
+        int n = A.length;
+        int[] ans = new int[n];
+
+        for(int i=n-1;i>=0;i--) {
+            while(!st.isEmpty() && A[st.peek()] <= A[i]) {
+                st.pop();
+            }
+            if(st.isEmpty()) {
+                ans[i] = n;
+            } else {
+                ans[i] = st.peek();
+            }
+            st.push(i);
+        }
+        return ans;
+    }
+    public int[] nearestSmallerLeft(int[] A) {
+        Stack<Integer> st = new Stack();
+        int n = A.length;
+        int[] ans = new int[n];
+
+        for(int i=0;i<n;i++) {
+            while(!st.isEmpty() && A[st.peek()] >= A[i]) {
+                st.pop();
+            }
+            if(st.isEmpty()) {
+                ans[i] = -1;
+            } else {
+                ans[i] = st.peek();
+            }
+            st.push(i);
+        }
+        return ans;
+    }
+    public int[] nearestSmallerRight(int[] A) {
+        Stack<Integer> st = new Stack();
+        int n = A.length;
+        int[] ans = new int[n];
+
+        for(int i=n-1;i>=0;i--) {
+            while(!st.isEmpty() && A[st.peek()] >= A[i]) {
+                st.pop();
+            }
+            if(st.isEmpty()) {
+                ans[i] = n;
+            } else {
+                ans[i] = st.peek();
+            }
+            st.push(i);
+        }
+        return ans;
+    }
+    public int[] nglFunc(int[] A) {
+
+        Stack<Integer> stack=new Stack();
+
+        int ans[]=new int[A.length];
+
+        for(int i=0;i<A.length;i++){
+
+            while(stack.size()!=0 && A[stack.peek()]<=A[i]){ stack.pop(); }
+
+            if(stack.size()==0) ans[i]=-1;
+
+            else ans[i]=stack.peek();
+
+            stack.push(i);
+
+        }
+
+        return ans;
+
+    }
+    public int solve(int[] A) {
+        long mod = 1L*1000000007;
+        long ans = 0;
+        int n = A.length;
+        int[] smallerLeft = new int[n];
+        int[] smallerRight = new int[n];
+        int[] greaterLeft = new int[n];
+        int[] greaterRight = new int[n];
+        smallerLeft = nearestSmallerLeft(A);
+        smallerRight = nearestSmallerRight(A);
+        greaterLeft = nearestGreaterLeft(A);
+        greaterRight = nearestGreaterRight(A);
+        for(int i=0; i<n;i++) {
+            long max=1L*(i-greaterLeft[i])*(greaterRight[i]-i);
+            long min=1L*(i-smallerLeft[i])*(smallerRight[i]-i);
+            ans+=(max-min)*A[i];
+        }
+        
+        return (int)(ans%mod);
+    }
+}
+
+// Solution by team
+// public class Solution {
+//     int[] a;
+//     public void findNextGreaterElement(int[] Next_greater_element, int n){
+//         // this function calculates next_greater element index
+//         Stack < Integer > s = new Stack < Integer > ();
+//         for (int i = 0; i < n; i++) Next_greater_element[i + 1] = n + 1;
+//         for (int i = 1; i <= n; i++) { 
+//             if (s.empty()) {
+//                 s.push(i);
+//             } else {
+//                 while (!s.empty() && a[s.peek()] <= a[i]) {
+//                     Next_greater_element[s.peek()] = i;
+//                     s.pop();
+//                 }
+//                 s.push(i);
+//             }
+//         }
+//     }
+//     public void findPreviousGreaterElement(int[] Previous_greater_element, int n){
+//         // this function calculates Previous_greater element index
+//         Stack < Integer > s = new Stack < Integer > ();
+//         for (int i = n; i > 0; i--) { 
+//             if (s.empty()) {
+//                 s.push(i);
+//             } else {
+//                 while (!s.empty() && a[i] > a[s.peek()]) {
+//                     Previous_greater_element[s.peek()] = i;
+//                     s.pop();
+//                 }
+//                 s.push(i);
+//             }
+//         }
+//     }
+//     public void findPreviousSmallerElement(int[] Previous_smaller_element, int n){
+//         // this function calculates Previous smaller element index
+//         Stack < Integer > s = new Stack < Integer > ();
+//         for (int i = n; i > 0; i--) { 
+//             if (s.empty()) {
+//                 s.push(i);
+//             } else {
+//                 while (!s.empty() && a[i] <= a[s.peek()]) {
+//                     Previous_smaller_element[s.peek()] = i;
+//                     s.pop();
+//                 }
+//                 s.push(i);
+//             }
+//         }
+//     }
+//     public void findNextSmallerElement(int[] Next_smaller_element, int n){
+//         // function function calculates Next smaller element index
+//         Stack < Integer > s = new Stack < Integer > ();
+//         for (int i = 0; i < n; i++) Next_smaller_element[i + 1] = n + 1;
+//         for (int i = 1; i <= n; i++) { 
+//             if (s.empty()) {
+//                 s.push(i);
+//             } else {
+//                 while (!s.empty() && a[i] < a[s.peek()]) {
+//                     Next_smaller_element[s.peek()] = i;
+//                     s.pop();
+//                 }
+//                 s.push(i);
+//             }
+//         }
+//     }
+    
+//     public int solve(int[] A) {
+//         int n = A.length, mod = 1000 * 1000 * 1000 + 7;
+//         a = new int[n + 1];
+//         int Next_greater_element[] = new int[n + 1];
+//         int Previous_greater_element[] = new int[n + 1];
+//         int Previous_smaller_element[] = new int[n + 1];
+//         int Next_smaller_element[] = new int[n + 1];
+//         for (int i = 0; i < n; i++) {
+//             a[i + 1] = A[i];
+//         }
+        
+//         findNextGreaterElement(Next_greater_element, n);
+//         findPreviousGreaterElement(Previous_greater_element, n);
+//         findPreviousSmallerElement(Previous_smaller_element, n);
+//         findNextSmallerElement(Next_smaller_element, n);
+       
+//         long ans = 0;
+//         for (int i = 1; i <= n; i++) {
+//             long right = Next_greater_element[i] - i;
+//             long left = i - Previous_greater_element[i];
+//             ans += (((left * right) % mod) * a[i]) % mod;
+//             ans %= mod;
+//             left = i - Previous_smaller_element[i];
+//             right = Next_smaller_element[i] - i;
+//             ans -= (((left * right) % mod) * a[i]) % mod;
+//             ans += mod;
+//             ans %= mod;
+//         }
+//         return (int) ans;
+//     }
+// }
+
+
 public class Stacks2 {
+
+
+
     // Largest Rectangle in Histogram
     // Given an array of integers A.
     //A represents a histogram i.e A[i] denotes the height of the ith histogram's bar. Width of each bar is 1.
