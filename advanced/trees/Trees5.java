cc80690ab15f3023aec91bbd98b6850f1ba6f200
Ronit Nandwani <ronitnandwani@gmail.com>
2024-03-30 07:36:00 +0100

    Modified: trees 5 added problem next pointer binart tree

--- advanced/trees/Trees5.java
+++ advanced/trees/Trees5.java
@@ -1,9 +1,111 @@
 package advanced.trees;
 
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.Map;
+import java.util.Queue;
+
+class TreeLinkNode {
+        int val;
+     TreeLinkNode left, right, next;
+        TreeLinkNode(int x) { val = x; }
+    }
 
 public class Trees5 {
+    // Next Pointer Binary tree
+
+    // Given a binary tree,
+    // Populate each next pointer to point to its next right node. If there is no next right node, the next pointer should be set to NULL.
+    // Initially, all next pointers are set to NULL.
+    // Assume perfect binary tree.
+    // Problem Constraints
+    // 1 <= Number of nodes in binary tree <= 100000
+    // 0 <= node values <= 10^9
+    // Input Format
+    // First and only argument is head of the binary tree A.
+    // Output Format
+    // Return the head of the binary tree after the changes are made.
+    // Example Input
+    // Input 1:
+    //     1
+    //     /  \
+    // 2    3
+    // Input 2:
+    //         1
+    //     /  \
+    //     2    5
+    //     / \  / \
+    //     3  4  6  7
+    // Example Output
+    // Output 1:
+    //         1 -> NULL
+    //     /  \
+    //     2 -> 3 -> NULL
+    // Output 2:
+    //         1 -> NULL
+    //     /  \
+    //     2 -> 5 -> NULL
+    //     / \  / \
+    //     3->4->6->7 -> NULL
+    // Example Explanation
+    // Explanation 1:
+    // Next pointers are set as given in the output.
+    // Explanation 2:
+
+    // Next pointers are set as given in the output.
+    public void connect(TreeLinkNode root) {
+        TreeLinkNode temp = root;
+        TreeLinkNode last = root;
+        TreeLinkNode r = root;
+
+        while(r != null) {
+            if(r.left != null) {
+                temp.next = r.left;
+                temp = temp.next;
+            }
+
+            if(r.right != null) {
+                temp.next = r.right;
+                temp = temp.next;
+            }
+
+            if(r != last) {
+                r = r.next;
+            } else {
+                r = last.next;
+                last.next = null;
+                last = temp;
+            }
+        }
+    }
+    // Solution by team
+    // public class Solution {
+    //     public void connect(TreeLinkNode root) {
+    //         if (root == null)
+    //             return;
+
+    //         Queue < TreeLinkNode > q = new LinkedList < TreeLinkNode > ();
+    //         q.offer(root);
+
+    //         while (q.size() > 0) {
+    //             int n = q.size();
+    //             for (int i = 0; i < n; i++) {
+    //                 TreeLinkNode front = q.poll();
+    //                 if (i == n - 1) {
+    //                     front.next = null;
+    //                 } else {
+    //                     front.next = q.peek();
+    //                 }
+    //                 if (front.left != null)
+    //                     q.offer(front.left);
+    //                 if (front.right != null)
+    //                     q.offer(front.right);
+    //             }
+    //         }
+    //         return;
+    //     }
+    // }
+
     // Diameter of binary Tree
     
     // Given a Binary Tree A consisting of N integer nodes, you need to find the diameter of the tree.
