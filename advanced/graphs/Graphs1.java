d0bc31ddf3c395c452da1414ac084f07fb9de9e5
Ronit Nandwani <ronitnandwani@gmail.com>
2024-04-14 07:36:00 +0100

    Modified: graphs 1 to add problem for no. of islands

--- advanced/graphs/Graphs1.java
+++ advanced/graphs/Graphs1.java
@@ -7,6 +7,140 @@ import java.util.Map;
 import java.util.Queue;
 
 public class Graphs1 {
+    // Number of islands
+    // Given a matrix of integers A of size N x M consisting of 0 and 1. A group of connected 1's forms an island. From a cell (i, j) such that A[i][j] = 1 you can visit any cell that shares a corner with (i, j) and value in that cell is 1.
+    // More formally, from any cell (i, j) if A[i][j] = 1 you can visit:
+    // (i-1, j) if (i-1, j) is inside the matrix and A[i-1][j] = 1.
+    // (i, j-1) if (i, j-1) is inside the matrix and A[i][j-1] = 1.
+    // (i+1, j) if (i+1, j) is inside the matrix and A[i+1][j] = 1.
+    // (i, j+1) if (i, j+1) is inside the matrix and A[i][j+1] = 1.
+    // (i-1, j-1) if (i-1, j-1) is inside the matrix and A[i-1][j-1] = 1.
+    // (i+1, j+1) if (i+1, j+1) is inside the matrix and A[i+1][j+1] = 1.
+    // (i-1, j+1) if (i-1, j+1) is inside the matrix and A[i-1][j+1] = 1.
+    // (i+1, j-1) if (i+1, j-1) is inside the matrix and A[i+1][j-1] = 1.
+    // Return the number of islands.
+
+    // NOTE: Rows are numbered from top to bottom and columns are numbered from left to right.
+
+    // Problem Constraints
+    // 1 <= N, M <= 100
+
+    // 0 <= A[i] <= 1
+
+    // Input Format
+    // The only argument given is the integer matrix A.
+
+    // Output Format
+    // Return the number of islands.
+
+    // Example Input
+    // Input 1:
+
+    // A = [ 
+    //     [0, 1, 0]
+    //     [0, 0, 1]
+    //     [1, 0, 0]
+    //     ]
+    // Input 2:
+
+    // A = [   
+    //     [1, 1, 0, 0, 0]
+    //     [0, 1, 0, 0, 0]
+    //     [1, 0, 0, 1, 1]
+    //     [0, 0, 0, 0, 0]
+    //     [1, 0, 1, 0, 1]    
+    //     ]
+
+
+    // Example Output
+    // Output 1:
+
+    // 2
+    // Output 2:
+
+    // 5
+
+
+    // Example Explanation
+    // Explanation 1:
+
+    // The 1's at position A[0][1] and A[1][2] forms one island.
+    // Other is formed by A[2][0].
+    // Explanation 2:
+
+    // There 5 island in total.
+    int[][] visited1 = new int[1][1];
+    int n = 0;
+    int m = 0;
+    int[] rows = new int[]{0,-1,0,1,-1,-1,1,1};
+    int[] cols = new int[]{-1,0,1,0,-1,1,1,-1};
+    public int numberOfIslands(int[][] A) {
+        
+        int count = 0;
+        n = A.length;
+        m = A[0].length;
+        visited1 = new int[n][m];
+        for(int i=0;i<n;i++) {
+            for(int j=0;j<m;j++) {
+                if(visited1[i][j] == 0 && A[i][j] == 1) {
+                    count++;
+                    dfs(i,j, A);
+                }
+            }
+        }
+        return count;
+    }
+
+    public void dfs(int i, int j, int[][] A) {
+        visited1[i][j] = 1;
+        
+        for(int k=0;k<8;k++) {
+            int x = i+rows[k];
+            int y = j+cols[k];
+
+            if(x >= 0 && x < n && y>=0&&y<m&&visited1[x][y] == 0 && A[x][y]==1) {
+                dfs(x,y, A);
+            }
+        }
+    }
+    // Solution by team
+    public class Solution {
+        static int[] dx = new int[] {0, 1, -1, 0, 1, -1, 1, -1};
+        static int[] dy = new int[] {1, 0, 0, -1, -1, 1, 1, -1};
+        static int tc = 0;
+        static int[][] visited = new int[105][105];
+        public int solve(int[][] A) {
+            int n = A.length;
+            int m = A[0].length;
+            ++tc;
+            int ans = 0;
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < m; j++) {
+                    if (A[i][j] == 1 && visited[i][j] != tc) {
+                        dfs(i, j, n, m, A);
+                        ans++;
+                    }
+                }
+            }
+            return ans;
+        }
+        public static boolean check(int i, int j, int n, int m, int[][] A) {
+            return (i >= 0 && i < n && j >= 0 && j < m && (A[i][j] == 1) && visited[i][j] != tc);
+        }
+        public static void dfs(int i, int j, int n, int m, int[][] A) {
+            visited[i][j] = tc;
+            int di, dj;
+            for (int k = 0; k < 8; ++k) {
+                di = i + dx[k];
+                dj = j + dy[k];
+                if (check(di, dj, n, m, A))
+                    dfs(di, dj, n, m, A);
+            }
+        }
+    }
+
+
+
     // Path in directed graph
 
     // Given an directed graph having A nodes labelled from 1 to A containing M edges given by matrix B of size M x 2such that there is a edge directed from node
