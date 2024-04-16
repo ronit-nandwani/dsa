eb4c880f77c39466a29a4b44cf5679c8d2106c0c
Ronit Nandwani <ronitnandwani@gmail.com>
2024-04-16 07:36:00 +0100

    Modified: dp4 added problem coin sum infinite

--- advanced/dp/DP4.java
+++ advanced/dp/DP4.java
@@ -1,6 +1,86 @@
 package advanced.dp;
 
+import java.util.Arrays;
+
 public class DP4 {
+    // Coin sum infinite
+
+    //     You are given a set of coins A. In how many ways can you make sum B assuming you have infinite amount of each coin in the set.
+    // NOTE:
+    // Coins in set A will be unique. Expected space complexity of this problem is O(B).
+    // The answer can overflow. So, return the answer % (106 + 7).
+
+    // Problem Constraints
+    // 1 <= A <= 500
+    // 1 <= A[i] <= 1000
+    // 1 <= B <= 50000
+
+    // Input Format
+    // First argument is an integer array A representing the set.
+    // Second argument is an integer B.
+
+    // Output Format
+    // Return an integer denoting the number of ways.
+
+    // Example Input
+    // Input 1:
+
+    // A = [1, 2, 3]
+    // B = 4
+    // Input 2:
+
+    // A = [10]
+    // B = 10
+    // Example Output
+    // Output 1:
+    // 4
+    // Output 2:
+    // 1
+    // Example Explanation
+    // Explanation 1:
+    // The 4 possible ways are:
+    // {1, 1, 1, 1}
+    // {1, 1, 2}
+    // {2, 2}
+    // {1, 3} 
+    // Explanation 2:
+
+    // There is only 1 way to make sum 10.
+
+    public int coinchange2(int[] A, int B) {
+        int n = A.length;
+        int[] dp = new int[B+1];
+        dp[0] = 1;
+        for(int i=0;i<n;i++) {
+            for(int j=1;j<=B;j++) {
+                if(A[i] <= j)
+                    dp[j] += dp[j-A[i]];
+                    dp[j] = dp[j]%1000007;
+            }
+        }
+        return dp[B];
+    }
+
+    // Solution by team
+    public class Solution {
+        public int coinchange2(int[] A, int B) {
+            int[] num_ways = new int[B + 1];
+            int Mod = 1000000 + 7;
+            int i, j, m = A.length;
+            Arrays.fill(num_ways, 0);
+            num_ways[0] = 1;
+
+            for (i = 0; i < m; i++) {
+                for (j = A[i]; j <= B; j++) {
+                    num_ways[j] += num_ways[j - A[i]];
+                    num_ways[j] %= Mod;
+
+                }
+            }
+            return num_ways[B];
+        }
+    }
+
     // 0-1 Knapsack 2
     // Given two integer arrays A and B of size N each which represent values and weights associated with N items respectively.
     // Also given an integer C which represents knapsack capacity.
