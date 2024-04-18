becdae9c9b0d75d76064254844d8ea6673495e15
Ronit Nandwani <ronitnandwani@gmail.com>
2024-04-18 07:36:00 +0100

    Modified: dp3 added problem 0to1 knapsack

--- advanced/dp/DP3.java
+++ advanced/dp/DP3.java
@@ -3,6 +3,144 @@ package advanced.dp;
 import java.util.Arrays;
 
 public class DP3 {
+    // 0-1 knapsack
+
+    // Given two integer arrays A and B of size N each which represent values and weights associated with N items respectively.
+    // Also given an integer C which represents knapsack capacity.
+    // Find out the maximum value subset of A such that sum of the weights of this subset is smaller than or equal to C.
+
+    // NOTE:
+
+    // You cannot break an item, either pick the complete item, or don’t pick it (0-1 property).
+
+    // Problem Constraints
+    // 1 <= N <= 103
+
+    // 1 <= C <= 103
+
+    // 1 <= A[i], B[i] <= 103
+
+    // Input Format
+    // First argument is an integer array A of size N denoting the values on N items.
+
+    // Second argument is an integer array B of size N denoting the weights on N items.
+
+    // Third argument is an integer C denoting the knapsack capacity.
+
+    // Output Format
+    // Return a single integer denoting the maximum value subset of A such that sum of the weights of this subset is smaller than or equal to C.
+
+    // Example Input
+    // Input 1:
+
+    // A = [60, 100, 120]
+    // B = [10, 20, 30]
+    // C = 50
+    // Input 2:
+
+    // A = [10, 20, 30, 40]
+    // B = [12, 13, 15, 19]
+    // C = 10
+
+    // Example Output
+    // Output 1:
+
+    // 220
+    // Output 2:
+
+    // 0
+
+    // Example Explanation
+    // Explanation 1:
+
+    // Taking items with weight 20 and 30 will give us the maximum value i.e 100 + 120 = 220
+    // Explanation 2:
+
+    // Knapsack capacity is 10 but each item has weight greater than 10 so no items can be considered in the knapsack therefore answer is 0.
+    public int zeroto1Knapsack(int[] A, int[] B, int C) {
+        int n = A.length;
+        int m = B.length;
+
+        int[][] dp = new int[n+1][C+1];
+    
+        for(int i=1;i<=n;i++) {
+            for(int j=1;j<=C;j++) {
+                if(B[i-1] <= j) {
+                    dp[i][j] = Math.max(dp[i-1][j], A[i-1]+dp[i-1][j-B[i-1]]);
+                } else {
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+        }
+        return dp[n][C];
+    }
+
+
+    // Solution by team
+    public class Solution {
+        static int maxn = 1009;
+        static int[][] dp = new int[maxn][maxn];
+        public static int knapsack(int[] A, int[] B, int C) {
+            for (int[] row: dp)
+                Arrays.fill(row, 0);
+            int n = A.length;
+            for (int i = 1; i <= n; i++) {
+                for (int j = 1; j <= C; j++) {
+                    dp[i][j] = dp[i - 1][j];
+                    if (B[i - 1] <= j)
+                        dp[i][j] = Math.max(dp[i][j], A[i - 1] + dp[i - 1][j - B[i - 1]]);
+                }
+            }
+            return dp[n][C];
+        }
+        public int solve(int[] A, int[] B, int C) {
+            return knapsack(A, B, C);
+        }
+    }
+    // Solution using recursion memoization
+    public class Solution1 {
+
+        int[] pft; int[] wt; int cap;
+    
+        int [][]dp;
+    
+        public int solve(int[] A, int[] B, int C) {
+    
+            dp= new int[A.length+1][C+1];
+    
+            for(int i=0;i<=A.length;i++){
+    
+                Arrays.fill(dp[i],-1);
+    
+            }
+    
+            pft=A; wt=B; cap=C;
+    
+            return knap(pft.length, cap);
+    
+        }
+    
+        int knap(int ele, int cap){
+    
+            if(ele==0 || cap==0) return 0;
+    
+            else if(dp[ele][cap]!=-1) return dp[ele][cap];
+    
+            else{
+    
+                int npick=knap(ele-1,cap);
+    
+                int pick=0;
+    
+                if(cap>=wt[ele-1]) pick=pft[ele-1]+knap(ele-1,cap-wt[ele-1]);
+    
+                return dp[ele][cap]=Math.max(pick,npick);
+    
+            }
+    
+        } 
+    }
+
     // Unbounded knapsack
     // Problem Description
     // Given a knapsack weight A and a set of items with certain value B[i] and weight C[i], we need to calculate maximum amount that could fit in this quantity.
