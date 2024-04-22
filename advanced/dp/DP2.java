7c160c77edd56d480b91f314a2c85c5c22ab18d1
Ronit Nandwani <ronitnandwani@gmail.com>
2024-04-22 07:36:00 +0100

    Modified: dp2 added problem to find min sum path in matrix

--- advanced/dp/DP2.java
+++ advanced/dp/DP2.java
@@ -4,6 +4,107 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class DP2 {
+
+    // Min Sum Path in Matrix
+
+    // Given a M x N grid A of integers, find a path from top left to bottom right which minimizes the sum of all numbers along its path.
+
+    // Return the minimum sum of the path.
+
+    // NOTE: You can only move either down or right at any point in time.
+    // Problem Constraints
+    // 1 <= M, N <= 2000
+
+    // -1000 <= A[i][j] <= 1000
+
+    // Input Format
+    // First and only argument is a 2-D grid A.
+
+    // Output Format
+    // Return an integer denoting the minimum sum of the path.
+
+    // Example Input
+    // Input 1:
+
+    // A = [
+    //     [1, 3, 2]
+    //     [4, 3, 1]
+    //     [5, 6, 1]
+    //     ]
+    // Input 2:
+
+    // A = [
+    //     [1, -3, 2]
+    //     [2, 5, 10]
+    //     [5, -5, 1]
+    //     ]
+
+    // Example Output
+    // Output 1:
+
+    // 8
+    // Output 2:
+
+    // -1
+
+    // Example Explanation
+    // Explanation 1:
+
+    // The path will be: 1 -> 3 -> 2 -> 1 -> 1.
+    // Input 2:
+
+    // The path will be: 1 -> -3 -> 5 -> -5 -> 1.
+
+    public int minPathSum(int[][] A) {
+        int m = A.length;
+        int n= A[0].length;
+        int[] dp = new int[n];
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i==0 && j==0)
+                    dp[j] = A[i][j];
+                else if(i==0)
+                    dp[j] = A[i][j] + dp[j-1];
+                else if(j==0)
+                    dp[j] = dp[j] + A[i][j];
+                else
+                    dp[j] = A[i][j] + Math.min(dp[j-1], dp[j]);
+            }
+        }
+        return dp[n-1];
+    }
+
+    // Solution by team
+
+    public class Solution {
+        private int mem[][];
+        private ArrayList < ArrayList < Integer >> A;
+        public int minPathSum(ArrayList < ArrayList < Integer >> A) {
+            int m, n;
+            if (A == null || A.size() == 0 || A.get(0).size() == 0)
+                return 0;
+            m = A.size();
+            n = A.get(0).size();
+            mem = new int[m][n];
+            for (int i = 0; i < m; i++)
+                Arrays.fill(mem[i], Integer.MAX_VALUE);
+            mem[0][0] = A.get(0).get(0);
+            this.A = A;
+            rec(m - 1, n - 1);
+            return mem[m - 1][n - 1];
+        }
+    
+        public int rec(int i, int j) {
+            if (i < 0 || j < 0)
+                return Integer.MAX_VALUE;
+            if (mem[i][j] != Integer.MAX_VALUE)
+                return mem[i][j];
+            int res = rec(i - 1, j);
+            res = Math.min(rec(i, j - 1), res);
+            mem[i][j] = res + A.get(i).get(j);
+            return mem[i][j];
+        }
+    }
     
     // N digit number
 
