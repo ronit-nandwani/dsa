260cb9336547d303ba1d80f3b7dbdfe96e9f6cfe
Ronit Nandwani <ronitnandwani@gmail.com>
2024-04-24 07:36:00 +0100

    Modified: LL1 added problem to remove duplicates from sorted list

--- advanced/linkedlist/LinkedList1.java
+++ advanced/linkedlist/LinkedList1.java
@@ -129,6 +129,97 @@ class ListNode {
 
 public abstract class LinkedList1 {
 
+    // Remove Duplicates from sorted LL
+    // Given a sorted linked list, delete all duplicates such that each element appears only once.
+
+    // Problem Constraints
+    // 0 <= length of linked list <= 106
+
+    // Input Format
+    // First argument is the head pointer of the linked list.
+
+    // Output Format
+    // Return the head pointer of the linked list after removing all duplicates.
+
+    // Example Input
+    // Input 1:
+
+    // 1->1->2
+    // Input 2:
+
+    // 1->1->2->3->3
+    // Example Output
+    // Output 1:
+
+    // 1->2
+    // Output 2:
+
+    // 1->2->3
+    // Example Explanation
+    // Explanation 1:
+
+    // Each element appear only once in 1->2.
+    public ListNode deleteDuplicates(ListNode A) {
+        // If the list is empty or has only one node, return it as it is
+      if(A == null || A.next == null){
+          return A;
+      }
+
+      // Pointer to traverse the list
+      ListNode temp = A;
+
+      // Traverse the list
+      while(temp != null && temp.next != null){
+              // If the current node's value is the same as the next node's value
+              if(temp.val == temp.next.val){
+                  // Skip the next node
+                  temp.next = temp.next.next;
+              }
+              else{
+                  // Move to the next node
+                  temp = temp.next;
+              }
+      }
+
+      // Return the head of the modified list
+      return A;
+
+  }
+  // Solution by team
+  /**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     ListNode(int x) { val = x; next = null; }
+ * }
+ */
+public class Solution {
+    public ListNode deleteDuplicates(ListNode A) {
+        if (A == null)
+            return A;
+        ListNode next;
+        ListNode prevNode;
+        int prev = A.val;
+        next = A.next;
+        prevNode = A;
+        while (next != null) {
+            if (next.val == prev) {
+                prevNode.next = next.next;
+            } else {
+                prevNode = next;
+                prev = next.val;
+            }
+            next = next.next;
+        }
+        return A;
+    }
+}
+
+
+
+    // Reverse a LL
+
     // You are given a singly linked list having head node A. You have to reverse
     // the linked list and return the head node of that reversed list.
     // NOTE: You have to do it in-place and in one-pass.
