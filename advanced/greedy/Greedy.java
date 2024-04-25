47cae6059908898c29b6ee818cc30fbde6d9fbfc
Ronit Nandwani <ronitnandwani@gmail.com>
2024-04-25 07:36:00 +0100

    Modified: greedy added problem another coin problem

--- advanced/greedy/Greedy.java
+++ advanced/greedy/Greedy.java
@@ -20,6 +20,98 @@ class JobPair {
     }
 }
 public class Greedy {
+    // Another coin problem
+    // The monetary system in DarkLand is really simple and systematic. The locals-only use coins. The coins come in different values. The values used are:
+
+    // 1, 5, 25, 125, 625, 3125, 15625, ...
+    // Formally, for each K >= 0 there are coins worth 5K.
+
+    // Given an integer A denoting the cost of an item, find and return the smallest number of coins necessary to pay exactly the cost of the item (assuming you have a sufficient supply of coins of each of the types you will need).
+
+    // Problem Constraints
+    // 1 <= A <= 2×109
+
+    // Input Format
+    // The only argument given is integer A.
+
+    // Output Format
+    // Return the smallest number of coins necessary to pay exactly the cost of the item.
+
+    // Example Input
+    // Input 1:
+
+    // A = 47
+    // Input 2:
+
+    // A = 9
+
+    // Example Output
+    // Output 1:
+
+    // 7
+    // Output 2:
+
+    // 5
+
+    // Example Explanation
+    // Explanation 1:
+
+    // Representation of 7 coins will be : (1 + 1 + 5 + 5 + 5 + 5 + 25).
+    // Explanation 2:
+
+    // Representation of 5 coins will be : (1 + 1 + 1 + 1 + 5).
+
+    public int anotherCoinProblem(int A) {
+        // Generate the coin values which are powers of 5
+        ArrayList<Integer> coinValues = new ArrayList<>();
+        int value = 1;
+        while (value <= A) {
+            coinValues.add(value);
+            if (value > Integer.MAX_VALUE / 5) {
+                break; // Prevent integer overflow
+            }
+            value *= 5;
+        }
+
+        // Reverse the list to start with the largest coin
+        Collections.reverse(coinValues);
+
+        int count = 0;
+        for (int coin : coinValues) {
+            if (A == 0) {
+                break;
+            }
+            count += A / coin; // Use as many coins of this value as possible
+            A %= coin;         // Reduce A by the value of coins used
+        }
+        return count;
+    }
+    // Solution by team
+    public class Solution {
+        public int solve(int A) {
+            ArrayList < Integer > v = new ArrayList < Integer > ();
+    
+            int val = 1;
+    
+            // Storing all denominations of coins
+            while (val <= 2000000000) {
+                v.add(val);
+                val = val * 5;
+            }
+            // Sort in decreasing order
+            Collections.reverse(v);
+    
+            int ans = 0;
+            // Loop from the largest denomination
+            for (int i = 0; i < v.size(); i++) {
+                ans += A / v.get(i);
+                A = A % v.get(i);
+            }
+            return ans;
+        }
+    }
+
+
     // Finish Maximum Jobs (Scheduling)
     // There are N jobs to be done, but you can do only one job at a time.
     // Given an array A denoting the start time of the jobs and an array B denoting the finish time of the jobs.
