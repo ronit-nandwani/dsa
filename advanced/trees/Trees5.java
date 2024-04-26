37c3b0110ad36766c8d396e881e8deca983666bd
Ronit Nandwani <ronitnandwani@gmail.com>
2024-04-26 07:36:00 +0100

    Modified: trees 5 added problem to invert binary tree

--- advanced/trees/Trees5.java
+++ advanced/trees/Trees5.java
@@ -12,6 +12,97 @@ class TreeLinkNode {
     }
 
 public class Trees5 {
+    // Invert binart tree
+
+    // Given a binary tree A, invert the binary tree and return it.
+
+    // Inverting refers to making the left child the right child and vice versa.
+
+    // Problem Constraints
+    // 1 <= size of tree <= 100000
+
+    // Input Format
+    // First and only argument is the head of the tree A.
+
+    // Output Format
+    // Return the head of the inverted tree.
+
+    // Example Input
+    // Input 1:
+    
+    //     1
+    // /   \
+    // 2     3
+    // Input 2:
+
+    //     1
+    // /   \
+    // 2     3
+    // / \   / \
+    // 4   5 6   7
+
+    // Example Output
+    // Output 1:
+    
+    //     1
+    // /   \
+    // 3     2
+    // Output 2:
+
+    //     1
+    // /   \
+    // 3     2
+    // / \   / \
+    // 7   6 5   4
+
+    // Example Explanation
+    // Explanation 1:
+
+    // Tree has been inverted.
+    // Explanation 2:
+
+    // Tree has been inverted.
+    public TreeNode invertTree(TreeNode A) {
+        if(A == null) return null;
+        // if(A.left == null && A.right == null) return A;
+
+        TreeNode inv_left = invertTree(A.left);
+        TreeNode inv_right = invertTree(A.right);
+
+        A.left = inv_right;
+        A.right = inv_left;
+
+        return A;
+    }
+
+    // Solution by team
+    /**
+     * Definition for binary tree
+     * class TreeNode {
+     *     int val;
+     *     TreeNode left;
+     *     TreeNode right;
+     *     TreeNode(int x) {
+     *      val = x;
+     *      left=null;
+     *      right=null;
+     *     }
+     * }
+     */
+    public class Solution {
+        public TreeNode invertTree(TreeNode root) {
+            if (root == null) {
+                return null;
+            }
+            TreeNode right = invertTree(root.right);
+            TreeNode left = invertTree(root.left);
+            root.left = right;
+            root.right = left;
+            return root;
+        }
+    }
+
+
     // Next Pointer Binary tree
 
     // Given a binary tree,
