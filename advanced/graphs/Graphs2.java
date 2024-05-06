f665073f455106b66f6270797787797f5f48c103
Ronit Nandwani <ronitnandwani@gmail.com>
2024-05-06 17:38:00 +0100

    Modified: graphs 2 added topological sort problem

--- advanced/graphs/Graphs2.java
+++ advanced/graphs/Graphs2.java
@@ -1,10 +1,180 @@
 package advanced.graphs;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.LinkedList;
+import java.util.List;
+import java.util.PriorityQueue;
 import java.util.Queue;
 
 public class Graphs2 {
+
+    // Topological Sort
+
+    // Given an directed acyclic graph having A nodes. A matrix B of size M x 2 is given which represents the M edges such that there is a edge directed from node B[i][0] to node B[i][1].
+    // Topological sorting for Directed Acyclic Graph (DAG) is a linear ordering of vertices such that for every directed edge uv, vertex u comes before v in the ordering. Topological Sorting for a graph is not possible if the graph is not a DAG.
+
+    // Return the topological ordering of the graph and if it doesn't exist then return an empty array.
+
+    // If there is a solution return the correct ordering. If there are multiple solutions print the lexographically smallest one.
+
+    // Ordering (a, b, c) is said to be lexographically smaller than ordering (e, f, g) if a < e or if(a==e) then b < f and so on.
+
+    // NOTE:
+
+    // There are no self-loops in the graph.
+    // The graph may or may not be connected.
+    // Nodes are numbered from 1 to A.
+    // Your solution will run on multiple test cases. If you are using global variables make sure to clear them.
+
+
+    // Problem Constraints
+    // 2 <= A <= 104
+
+    // 1 <= M <= min(100000,A*(A-1))
+
+    // 1 <= B[i][0], B[i][1] <= A
+
+
+
+    // Input Format
+    // The first argument given is an integer A representing the number of nodes in the graph.
+
+    // The second argument given a matrix B of size M x 2 which represents the M edges such that there is a edge directed from node B[i][0] to node B[i][1].
+
+
+
+    // Output Format
+    // Return a one-dimensional array denoting the topological ordering of the graph and it it doesn't exist then return empty array.
+
+
+
+    // Example Input
+    // Input 1:
+
+    // A = 6
+    // B = [  [6, 3] 
+    //         [6, 1] 
+    //         [5, 1] 
+    //         [5, 2] 
+    //         [3, 4] 
+    //         [4, 2] ]
+    // Input 2:
+
+    // A = 3
+    // B = [  [1, 2]
+    //         [2, 3] 
+    //         [3, 1] ]
+
+
+    // Example Output
+    // Output 1:
+
+    // [5, 6, 1, 3, 4, 2]
+    // Output 2:
+
+    // []
+
+
+    // Example Explanation
+    // Explanation 1:
+
+    // The given graph contain no cycle so topological ordering exists which is [5, 6, 1, 3, 4, 2]
+    // Explanation 2:
+
+    // The given graph contain cycle so topological ordering not possible we will return empty array.
+
+    List<List<Integer>> list;
+    public int[] solve(int A, int[][] B) {        
+        int []indegree = new int[A+1];
+        buildGraph(A,B,indegree);
+        return topologicalOrder(A,indegree);
+    }
+
+   private int[] topologicalOrder(int n,int []indegree){
+       int[] ans = new int[n];
+       int k=0;
+       PriorityQueue<Integer> q = new PriorityQueue();
+       for(int i=1;i<=n;i++){
+           if(indegree[i]==0){
+               q.add(i);
+           }
+       }
+       while(!q.isEmpty()){
+           int curr = q.poll();
+           ans[k]=curr;
+           k++;
+           List<Integer> nghbr = list.get(curr);
+           for(int v:nghbr){
+               indegree[v]--;
+               if(indegree[v]==0){
+                   q.add(v);
+               }
+           }
+       }
+       if(Arrays.stream(ans).anyMatch(i->i!=0)){
+          return ans;
+       }
+       return new int[0];
+
+    }
+    private void buildGraph(int n, int[][] edges, int[] indegree){
+        list = new ArrayList();
+        for(int i=0;i<=n;i++){
+            list.add(new ArrayList());
+        }
+        for(int i=0;i<edges.length;i++){
+            int u=edges[i][0];
+            int v=edges[i][1];
+            list.get(u).add(v);
+            indegree[v]++;
+        }
+    }
+
+    // Solution by team
+
+    public class Solution {
+        static int maxn = 10009;
+        static int[] in = new int[maxn];
+        static ArrayList < ArrayList < Integer > > adj;
+        public static void graph() {
+            adj = new ArrayList < ArrayList < Integer > > (maxn);
+            for (int i = 0; i < maxn; i++) {
+                in [i] = 0;
+                adj.add(new ArrayList < Integer > ());
+            }
+        }
+        public int[] solve(int A, int[][] B) {
+            graph();
+            for (int[] edge: B) {
+                adj.get(edge[0]).add(edge[1]); 
+                in[edge[1]]++;
+            }
+            PriorityQueue < Integer > pq = new PriorityQueue < > ();
+            for (int i = 1; i <= A; i++) {
+                if ( in [i] == 0)
+                    pq.offer(i);
+            }
+            ArrayList < Integer > ans = new ArrayList < Integer > ();
+            while (pq.size() > 0) {
+                int temp = pq.poll();
+                ans.add(temp);
+                for (int v: adj.get(temp)) {
+                    in [v]--;
+                    if (in[v] == 0)
+                        pq.offer(v);
+                }
+            }
+            if (ans.size() != A)
+                ans.clear();
+            int[] res = new int[ans.size()];
+            for (int i = 0; i < ans.size(); i++)
+                res[i] = ans.get(i);
+            return res;
+        }
+    }
+
+
 
     // Possibility of Finishing
 
