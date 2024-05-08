efa76ed29fb1c6058cdec090b1b77e126feedccd
Ronit Nandwani <ronitnandwani@gmail.com>
2024-05-08 17:38:00 +0100

    Modified: heap1 problem for heap queries added

--- advanced/heaps/Heaps1.java
+++ advanced/heaps/Heaps1.java
@@ -7,6 +7,116 @@ import java.util.*;
       ListNode(int x) { val = x; next = null; }
  }
 public class Heaps1 {
+    // Heap Queries
+
+    // You have an empty min heap. You are given an array A consisting of N queries. Let P denote A[i][0] and Q denote A[i][1]. There are two types of queries:
+    // P = 1, Q = -1 : Pop the minimum element from the heap.
+    // P = 2, 1 <= Q <= 109 : Insert Q into the heap.
+    // Return an integer array containing the answer for all the extract min operation. If the size of heap is 0, then extract min should return -1.
+    // Problem Constraints
+    // 1 <= N <= 105
+    // 1 <= A[i][0] <= 2
+    // 1 <= A[i][1] <= 109 or A[i][1] = -1
+    // Input Format
+    // The only argument A is a 2D integer array
+    // Output Format
+    // Return an integer array
+    // Example Input
+    // Input 1:
+    // A = [[1, -1], [2, 2], [2, 1], [1, -1]]
+    // Input 2:
+    // A = [[2, 5], [2, 3], [2, 1], [1, -1], [1, -1]]
+    // Example Output
+    // Output 1:
+    // [-1, 1]
+    // Output 2:
+    // [1, 3]
+    // Example Explanation
+    // Explanation 1:
+    // For the first extract operation the heap is empty so it gives -1. For the second extract operation the heap contains the elements 2 and 1. Extract min returns the element 1.
+    // Explanation 2:
+
+    // The heap contains the elements 5, 3 and 1. The first extract min operation gets the element 1 and the second operation gets the element 3.
+    public ArrayList<Integer> heapQueries(ArrayList<ArrayList<Integer>> A) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        ArrayList<Integer> arr = new ArrayList<Integer>();
+        for(int i=0;i<A.size();i++) {
+            if(A.get(i).get(0) == 1) {
+                if(pq.isEmpty()) {
+                    arr.add(-1);
+                } else {
+                    arr.add(pq.remove());
+                }
+            } else if(A.get(i).get(0) == 2) {
+                pq.add(A.get(i).get(1));
+            }
+        }
+        return arr;
+    }
+    // Solution by team
+    // public class Solution {
+    //     private static void insertMinHeap(ArrayList<Integer> heap, int value) {
+    //         heap.add(value);
+    //         int index = heap.size() - 1;
+    //         while (index > 0 && heap.get((index - 1) / 2) > heap.get(index)) {
+    //             int temp = heap.get(index);
+    //             heap.set(index, heap.get((index - 1) / 2));
+    //             heap.set((index - 1) / 2, temp);
+    //             index = (index - 1) / 2;
+    //         }
+    //     }
+    
+    //     private static int extractMin(ArrayList<Integer> heap) {
+    //         if (heap.isEmpty()) {
+    //             return -1;
+    //         }
+    
+    //         int minElement = heap.get(0);
+    //         heap.set(0, heap.get(heap.size() - 1));
+    //         heap.remove(heap.size() - 1);
+    
+    //         int index = 0;
+    //         while (true) {
+    //             int leftChild = 2 * index + 1;
+    //             int rightChild = 2 * index + 2;
+    //             int smallest = index;
+    
+    //             if (leftChild < heap.size() && heap.get(leftChild) < heap.get(smallest)) {
+    //                 smallest = leftChild;
+    //             }
+    
+    //             if (rightChild < heap.size() && heap.get(rightChild) < heap.get(smallest)) {
+    //                 smallest = rightChild;
+    //             }
+    
+    //             if (smallest != index) {
+    //                 int temp = heap.get(index);
+    //                 heap.set(index, heap.get(smallest));
+    //                 heap.set(smallest, temp);
+    //                 index = smallest;
+    //             } else {
+    //                 break;
+    //             }
+    //         }
+    
+    //         return minElement;
+    //     }
+    
+    //     public ArrayList<Integer> solve(ArrayList<ArrayList<Integer>> A) {
+    //         ArrayList<Integer> ans = new ArrayList<>();
+    //         ArrayList<Integer> heap = new ArrayList<>();
+    //         for (List<Integer> operation : A) {
+    //             if (operation.get(0) == 1) {
+    //                 ans.add(extractMin(heap));
+    //             } else {
+    //                 insertMinHeap(heap, operation.get(1));
+    //             }
+    //         }
+    //         return ans;
+    //     }
+    // }
+
+
     // Build a heap
 
     // Given an array A of N integers, convert that array into a min heap and return the array.
@@ -37,6 +147,49 @@ public class Heaps1 {
     // The Heap in the Array format is [-2, 5, 0, 13, 11, 19, 27, 31].
     // Some more possible heaps are  [-2, 0, 5, 13, 11, 27, 19, 31], [-2, 5, 0, 11, 27, 13, 19, 31], etc. 
     // You can return any possible Valid Heap Structure.
+    
+    class Solution {
+        public int[] buildHeap(int[] A) {
+            // code here
+            int n = A.length;
+    
+            for(int i = (n-2)/2;i>=0;i--) {
+                heapify(A,i);
+            }
+    
+            return A;
+        }
+    
+        public void heapify(int[] heap, int i) {
+            while((2*i+1) < heap.length) {
+                int x = Math.min(heap[i], heap[2*i+1]);
+    
+                if(2*i+2 < heap.length) {
+                    x = Math.min(x, heap[2*i+2]);
+                }
+    
+                if(x == heap[i]) {
+                    break;
+                } else if(x == heap[2*i+1]) {
+                    int t = heap[i];
+                    heap[i] = heap[2*i+1];
+                    heap[2*i+1] = t;
+    
+                    i=2*i+1;
+                } else {
+                    int t = heap[i];
+                    heap[i] = heap[2*i+2];
+                    heap[2*i+2] = t;
+    
+                    i=2*i+2;
+                }
+            }
+        }
+    }
+
+    // or
+    
+    
     public int[] buildHeap(int[] A) {
         // understand
         PriorityQueue<Integer> pq = new PriorityQueue<>();
@@ -135,6 +288,20 @@ public class Heaps1 {
             }
         }
         return head;
+
+        // or
+        // ListNode temp = new ListNode(-1);
+        // head = temp;
+
+        // while(pq.size()>0) {
+        //     temp.next = pq.remove();
+        //     temp=temp.next;
+        //     if(temp.next != null) {
+        //         pq.add(temp.next);
+        //     }
+        // }
+
+        // return head.next;
     }
     // Solution by them
     // public class Solution {
