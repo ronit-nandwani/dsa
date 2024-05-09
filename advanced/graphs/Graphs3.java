83dcfec6a58254571926abb51215e278440e6038
Ronit Nandwani <ronitnandwani@gmail.com>
2024-05-09 17:38:00 +0100

    Modified: Graphs 3 added problem to find Construction Cost

--- advanced/graphs/Graphs3.java
+++ advanced/graphs/Graphs3.java
@@ -3,6 +3,7 @@ package advanced.graphs;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.PriorityQueue;
+import java.util.Queue;
 
 class DPair {
     int node;
@@ -27,6 +28,180 @@ class CustomComp implements Comparator < Pair > {
     }
 }
 public class Graphs3 {
+    // Construction Cost
+
+    // Given a graph with A nodes and C weighted edges. Cost of constructing the graph is the sum of weights of all the edges in the graph.
+
+    // Find the minimum cost of constructing the graph by selecting some given edges such that we can reach every other node from the 1st node.
+
+    // NOTE: Return the answer modulo 109+7 as the answer can be large.
+
+    // Problem Constraints
+    // 1 <= A <= 100000
+    // 0 <= C <= 100000
+    // 1 <= B[i][0], B[i][1] <= N
+    // 1 <= B[i][2] <= 109
+
+
+
+    // Input Format
+    // First argument is an integer A.
+    // Second argument is a 2-D integer array B of size C*3 denoting edges. B[i][0] and B[i][1] are connected by ith edge with weight B[i][2]
+
+
+
+    // Output Format
+    // Return an integer denoting the minimum construction cost.
+
+
+
+    // Example Input
+    // Input 1:
+
+    // A = 3
+    // B = [   [1, 2, 14]
+    //         [2, 3, 7]
+    //         [3, 1, 2]   ]
+    // Input 2:
+
+    // A = 3
+    // B = [   [1, 2, 20]
+    //         [2, 3, 17]  ]
+
+
+    // Example Output
+    // Output 1:
+
+    // 9
+    // Output 2:
+
+    // 37
+
+
+    // Example Explanation
+    // Explanation 1:
+
+    // We can take only two edges (2 -> 3 and 3 -> 1) to construct the graph. 
+    // we can reach the 1st node from 2nd and 3rd node using only these two edges.
+    // So, the total cost of costruction is 9.
+    // Explanation 2:
+
+    // We have to take both the given edges so that we can reach the 1st node from 2nd and 3rd node.
+
+
+    ArrayList<int[]> [] adj;
+    Queue<int[]> heap;
+    boolean[] visited;
+
+    public int solve(int A, int[][] B) {
+        adj = new ArrayList[A + 1];
+        createAdj(B);        
+        heap = new PriorityQueue<>(Comparator.comparingInt(i -> i[2]));
+        visited = new boolean[A + 1];
+        visited[1] = true;
+        addNodes(1);
+        int count = 1;
+        long ans = 0;
+       
+        while (count < A) {
+            int[] node = heap.poll();
+            if (!visited[node[1]]) {
+                visited[node[1]] = true;
+                count++;
+                ans += node[2];
+                ans %= 1000000007;
+                addNodes(node[1]);      
+            }
+        }
+        return (int)ans;
+    }
+
+    private void createAdj(int[][] B) {
+        for (int i = 0; i < adj.length; i++) adj[i] = new ArrayList<>();
+        for (int i = 0; i < B.length; i++) {
+            adj[B[i][0]].add(new int[]{B[i][1], B[i][2]});
+            adj[B[i][1]].add(new int[]{B[i][0], B[i][2]});
+        }
+    }
+
+    private void addNodes(int source) {
+        for (int i = 0; i < adj[source].size(); i++) {
+            int[] node = adj[source].get(i);
+            if (!visited[node[0]]) heap.add(new int[]{source, node[0], node[1]});
+        }        
+    }
+
+    // Solution by team
+    public class Solution {
+        static int maxn = 100009;
+        static int[] arr = new int[maxn];
+        static int[] sz = new int[maxn];
+        static ArrayList < pair > edges;
+        static long mod = 1000000007;
+        public int solve(int A, int[][] B) {
+            ini();
+            for (int i = 0; i < B.length; i++)
+                edges.add(new pair(B[i][2], i));
+            Collections.sort(edges);
+            int ans = 0;
+            for (pair p: edges) {
+                int ind = p.second;
+                int val = p.first;
+                int u = B[ind][0];
+                int v = B[ind][1];
+                if (root(u) == root(v))
+                    continue;
+                else {
+                    un(u, v);
+                    ans += (int) val;
+                    ans %= mod;
+                }
+            }
+            return (int) ans;
+    
+        }
+        public static void ini() {
+            edges = new ArrayList < pair > ();
+            for (int i = 0; i < maxn; i++) {
+                arr[i] = i;
+                sz[i] = 1;
+            }
+        }
+        public static int root(int a) {
+            while (arr[a] != a) {
+                arr[a] = arr[arr[a]];
+                a = arr[a];
+            }
+            return a;
+        }
+        public static void un(int a, int b) {
+            int ra = root(a);
+            int rb = root(b);
+            if (sz[ra] <= sz[rb]) {
+                arr[ra] = rb;
+                sz[rb] += sz[ra];
+            } else {
+                arr[rb] = ra;
+                sz[ra] += sz[rb];
+            }
+        }
+    }
+    class pair implements Comparable < pair > {
+        int first;
+        int second;
+        public pair(int a, int b) {
+            this.first = a;
+            this.second = b;
+        }
+        @Override
+        public int compareTo(pair temp) {
+            return this.first - temp.first;
+        }
+    }
+
+
+
+
     // Dijkstra
     // Given a weighted undirected graph having A nodes and M weighted edges, and a source node C.
 
