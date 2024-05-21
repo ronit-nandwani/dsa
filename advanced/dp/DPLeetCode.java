260a9734ddc04062ac04dba8fc02d84274e972aa
Ronit Nandwani <ronitnandwani@gmail.com>
2024-05-21 08:58:00 +0100

    Modified: DP Leetcode added problem "650. 2 Keys Keyboard"

--- advanced/dp/DPLeetCode.java
+++ advanced/dp/DPLeetCode.java
@@ -3,7 +3,70 @@ package advanced.dp;
 import java.util.Arrays;
 
 public class DPLeetCode {
+    // 650. 2 Keys Keyboard
+    // Solved
+    // Medium
+    // Topics
+    // Companies
+    // Hint
+    // There is only one character 'A' on the screen of a notepad. You can perform one of two operations on this notepad for each step:
+
+    // Copy All: You can copy all the characters present on the screen (a partial copy is not allowed).
+    // Paste: You can paste the characters which are copied last time.
+    // Given an integer n, return the minimum number of operations to get the character 'A' exactly n times on the screen.
+
     
+
+    // Example 1:
+
+    // Input: n = 3
+    // Output: 3
+    // Explanation: Initially, we have one character 'A'.
+    // In step 1, we use Copy All operation.
+    // In step 2, we use Paste operation to get 'AA'.
+    // In step 3, we use Paste operation to get 'AAA'.
+    // Example 2:
+
+    // Input: n = 1
+    // Output: 0
+    
+
+    // Constraints:
+
+    // 1 <= n <= 1000
+
+    // Solution by me - 0 ms
+    public int minSteps(int n) {
+        int result = 0;
+        for (int i = 2; i <= n; i++) {
+            while (n % i == 0) {
+                result += i;
+                n /= i;
+            }
+        }
+        return result;
+    }
+
+    // Faster Solution - 0 ms
+    public int minStepsFaster(int n) {
+        int steps = 0;
+        int maxDivider;
+        while(n>1){
+            maxDivider = getMaxDivider(n);
+            steps = n / maxDivider + steps;
+            n = maxDivider;
+        }
+        return steps;
+    }
+    private int getMaxDivider(int count){
+        int maxDivider = 1;
+        for(int i = maxDivider;i<count;i++){
+            if(count%i==0){
+                maxDivider = i;
+            }
+        }
+        return maxDivider;
+    }
 
     // ---------------------------------------------------
 
