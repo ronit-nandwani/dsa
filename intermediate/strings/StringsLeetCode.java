5d9ca26061f4d260c6bdc1baf697c89ff7f0e385
Ronit Nandwani <ronitnandwani@gmail.com>
2024-05-29 08:58:00 +0100

    Modified: strings leetcode added problem "592. Fraction Addition and Subtraction"

--- intermediate/strings/StringsLeetCode.java
+++ intermediate/strings/StringsLeetCode.java
@@ -4,6 +4,243 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class StringsLeetCode {
+    // 592. Fraction Addition and Subtraction
+    // Solved
+    // Medium
+    // Topics
+    // Companies
+    // Given a string expression representing an expression of fraction addition and subtraction, return the calculation result in string format.
+
+    // The final result should be an irreducible fraction. If your final result is an integer, change it to the format of a fraction that has a denominator 1. So in this case, 2 should be converted to 2/1.
+
+    
+
+    // Example 1:
+
+    // Input: expression = "-1/2+1/2"
+    // Output: "0/1"
+    // Example 2:
+
+    // Input: expression = "-1/2+1/2+1/3"
+    // Output: "1/3"
+    // Example 3:
+
+    // Input: expression = "1/3-1/2"
+    // Output: "-1/6"
+    
+
+    // Constraints:
+
+    // The input string only contains '0' to '9', '/', '+' and '-'. So does the output.
+    // Each fraction (input and output) has the format ±numerator/denominator. If the first input fraction or the output is positive, then '+' will be omitted.
+    // The input only contains valid irreducible fractions, where the numerator and denominator of each fraction will always be in the range [1, 10]. If the denominator is 1, it means this fraction is actually an integer in a fraction format defined above.
+    // The number of given fractions will be in the range [1, 10].
+    // The numerator and denominator of the final result are guaranteed to be valid and in the range of 32-bit int.
+
+    // Solution by me - 5 ms
+    public String fractionAddition(String expression) {
+        int numerator = 0;
+        int denominator = 1; // Initialize with 1 since we will be multiplying denominators
+        
+        int index = 0;
+        int length = expression.length();
+        
+        while (index < length) {
+            // Read the sign
+            int sign = 1;
+            if (expression.charAt(index) == '-') {
+                sign = -1;
+                index++;
+            } else if (expression.charAt(index) == '+') {
+                index++;
+            }
+            
+            // Read the numerator
+            int num = 0;
+            while (index < length && Character.isDigit(expression.charAt(index))) {
+                num = num * 10 + (expression.charAt(index) - '0');
+                index++;
+            }
+            
+            // Read the '/'
+            index++; // skip '/'
+            
+            // Read the denominator
+            int denom = 0;
+            while (index < length && Character.isDigit(expression.charAt(index))) {
+                denom = denom * 10 + (expression.charAt(index) - '0');
+                index++;
+            }
+            
+            // Apply the sign to the numerator
+            num *= sign;
+            
+            // Calculate the new numerator and denominator
+            numerator = numerator * denom + num * denominator;
+            denominator *= denom;
+            
+            // Simplify the fraction by GCD
+            int gcd = gcdGcd(Math.abs(numerator), Math.abs(denominator));
+            numerator /= gcd;
+            denominator /= gcd;
+        }
+        
+        return numerator + "/" + denominator;
+    }
+    
+    // Helper function to calculate GCD (Greatest Common Divisor)
+    private int gcdGcd(int a, int b) {
+        if (b == 0) return a;
+        return gcdGcd(b, a % b);
+    }
+
+    // Faster Solution - 0 ms
+
+    private static class Fraction {
+        int num;
+        int den;
+        Fraction(int n, int d) {
+            this.num = n;
+            this.den = d;
+        }
+    }
+    private void parseExpression(char[] expChArr, List<Fraction> fractionList) {
+         int i = 0;
+            while(i<expChArr.length) {
+                int num = 0, j = i, den = 0;
+                boolean numNeg = false, denNeg = false;
+
+                if(expChArr[j]=='-' || expChArr[j] == '+') {
+                    numNeg = expChArr[j]=='-';
+                    j++;
+                }
+                while(j < expChArr.length && expChArr[j]!='/') {
+                    int digit = (int)expChArr[j++] - 48;
+                    num = (num * 10) + digit;
+                }
+                j++;
+
+                if(expChArr[j]=='-' || expChArr[j] == '+') {
+                    denNeg = expChArr[j]=='-';
+                    j++;
+                }
+                while(j<expChArr.length && expChArr[j]!='-' && expChArr[j]!='+') {
+                    int digit = (int)expChArr[j++] - 48;
+                    den = (den * 10) + digit;
+                }
+
+                if(numNeg != denNeg) {
+                    num *= -1;
+                }
+
+                Fraction fraction = new Fraction(num, den);
+
+                fractionList.add(fraction);
+                i = j;
+            }
+    }
+
+    private Fraction evaluate(List<Fraction> fractions) {
+            Fraction prev = new Fraction(0, 1);
+            for(Fraction curr: fractions) {
+                int gcd = gcdHelper(prev.den, curr.den);
+                int lcm = (prev.den * curr.den)/gcd;
+
+                curr.num = curr.num * (lcm/curr.den);
+                prev.num = prev.num * (lcm/prev.den);
+
+                prev.num = prev.num + curr.num;
+                prev.den = lcm;
+            }
+
+            int gcd = gcdHelper(Math.abs(prev.num), prev.den);
+            prev.num = prev.num/gcd;
+            prev.den = prev.den/gcd;
+            return prev;
+        }
+
+        private int gcdHelper(int a, int b) {
+            return a > b ? gcd(a,b) : gcd(b,a);
+        }
+
+        private int gcd(int a, int b) {
+            if(b==0) return a;
+            return gcd(b, a % b);
+        }
+
+    public String fractionAdditionFaster(String expression) {
+
+        int rn = 0;
+        char[] cs = expression.toCharArray();
+        int[] sgns = new int[256];
+        sgns['-'] = -1;
+        sgns['+'] = 1;
+        int i = 0;
+        char sgn;
+        if (cs[0] == '-') {
+            sgn = '-';
+            i = 1;
+        } else if (cs[0] == '+') {
+            sgn = '+';
+            i = 1;
+        } else {
+            sgn = '+';
+        }
+        final int len = cs.length;
+        for (; ; i++) {
+            //int sign;
+            int n;
+            n = sgns[sgn] * (cs[i++] - '0');
+            if (cs[i] == '0') {
+                n *= 10;
+                i++;
+            }
+            int d = cs[++i] - '0';
+            i++;
+            if (i < len && cs[i] == '0') {
+                d = 10;
+                i++;
+            }
+            rn += n * 2520 / d;
+            if (i < len) {
+                sgn = cs[i];
+            } else {
+                break;
+            }
+        }
+        int dn = 2520;
+        int mod8 = (int)rn % 8;
+        switch (mod8) {
+            case 0: rn /= 8; dn = 315; break;
+            case 4: case -4: rn /= 4; dn = 630; break;
+            case 2: case 6: case -2: case -6: rn /= 2; dn = 1260; break;
+            default:
+        }
+        if (rn % 3 == 0) {
+            if (rn % 9 == 0) {
+                rn /= 9;
+                dn /= 9;
+            } else {
+                rn /= 3;
+                dn /= 3;
+            }
+        }
+        if (rn % 5 == 0) {
+            rn /= 5;
+            dn /= 5;
+        }
+        if (rn % 7 == 0) {
+            rn /= 7;
+            dn /= 7;
+        }
+        return new StringBuilder().append(rn).append("/").append(dn).toString();
+    }
+
+
+
+
+    // ---------------------------------------------
+
     // 564. Find the Closest Palindrome
     // Solved
     // Hard
