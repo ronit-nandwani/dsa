6cd536eed1a23138983ed715cc0b42478d5d22e2
Ronit Nandwani <ronitnandwani@gmail.com>
2024-05-30 08:58:00 +0100

    Modified: LL leetcode added problem "2807. Insert Greatest Common Divisors in Linked List"

--- advanced/linkedlist/LinkedListLeetCode.java
+++ advanced/linkedlist/LinkedListLeetCode.java
@@ -4,6 +4,122 @@ import java.util.HashSet;
 import java.util.List;
 
 public class LinkedListLeetCode {
+    // 2807. Insert Greatest Common Divisors in Linked List
+    // Solved
+    // Medium
+    // Topics
+    // Companies
+    // Given the head of a linked list head, in which each node contains an integer value.
+
+    // Between every pair of adjacent nodes, insert a new node with a value equal to the greatest common divisor of them.
+
+    // Return the linked list after insertion.
+
+    // The greatest common divisor of two numbers is the largest positive integer that evenly divides both numbers.
+
+    // Example 1:
+
+
+    // Input: head = [18,6,10,3]
+    // Output: [18,6,6,2,10,1,3]
+    // Explanation: The 1st diagram denotes the initial linked list and the 2nd diagram denotes the linked list after inserting the new nodes (nodes in blue are the inserted nodes).
+    // - We insert the greatest common divisor of 18 and 6 = 6 between the 1st and the 2nd nodes.
+    // - We insert the greatest common divisor of 6 and 10 = 2 between the 2nd and the 3rd nodes.
+    // - We insert the greatest common divisor of 10 and 3 = 1 between the 3rd and the 4th nodes.
+    // There are no more adjacent nodes, so we return the linked list.
+    // Example 2:
+
+
+    // Input: head = [7]
+    // Output: [7]
+    // Explanation: The 1st diagram denotes the initial linked list and the 2nd diagram denotes the linked list after inserting the new nodes.
+    // There are no pairs of adjacent nodes, so we return the initial linked list.
+    
+
+    // Constraints:
+
+    // The number of nodes in the list is in the range [1, 5000].
+    // 1 <= Node.val <= 1000
+
+
+    // Solutionn by me - 2 ms
+
+    public ListNode insertGreatestCommonDivisors(ListNode head) {
+        ListNode curr = head;
+        while(curr.next!=null) {
+            addGCDNode(curr);
+            curr = curr.next.next;
+        }
+        return head;
+    }
+
+    private void addGCDNode(ListNode curr) {
+        int currVal = curr.val;
+        int nextVal = curr.next.val;
+        ListNode temp = new ListNode(gcd(currVal, nextVal));
+
+        ListNode next = curr.next;
+        curr.next = temp;
+        temp.next = next;
+    }
+
+    public int gcd(int a, int b){
+        if(a == 1 || b==1){
+            return 1;
+        }
+        while(b !=0){
+            int rem = a % b;
+            a = b;
+            b= rem;
+        }
+        return a;
+    }
+
+    private int gcdOld(int a, int b)
+    {
+        // Find Minimum of a and b
+        int result = Math.min(a, b);
+        while (result > 0) {
+            if (a % result == 0 && b % result == 0) {
+                break;
+            }
+            result--;
+        }
+
+        // Return gcd of a and b
+        return result;
+    }
+
+    // Fastest solution - 1 ms
+
+    public ListNode insertGreatestCommonDivisorsFaster(ListNode head) {
+        
+        ListNode curr = head;
+
+        while(curr != null && curr.next != null) {
+            int hcf = gcdFaster(curr.val, curr.next.val);
+            ListNode temp = new ListNode(hcf);
+            temp.next = curr.next;
+            curr.next = temp;
+            curr = curr.next.next;
+        }
+
+        return head;
+    }
+
+
+    public int gcdFaster(int a, int b) {
+        if(b == 0) {
+            return a;
+        }
+        return gcd(b, a%b);
+    }
+
+
+    // ----------------------------------------------------------------
+
+
+
     // 725. Split Linked List in Parts
     // Solved
     // Medium
