5271e9e037974ba6beff73f2cbeeca3c8b20aba4
Ronit Nandwani <ronitnandwani@gmail.com>
2024-06-01 08:08:00 +0100

    Modified: LL leetcode added problem "1367. Linked List in Binary Tree"

--- advanced/linkedlist/LinkedListLeetCode.java
+++ advanced/linkedlist/LinkedListLeetCode.java
@@ -3,7 +3,106 @@ package advanced.linkedlist;
 import java.util.HashSet;
 import java.util.List;
 
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+
+
 public class LinkedListLeetCode {
+    // 1367. Linked List in Binary Tree
+    // Solved
+    // Medium
+    // Topics
+    // Companies
+    // Hint
+    // Given a binary tree root and a linked list with head as the first node. 
+
+    // Return True if all the elements in the linked list starting from the head correspond to some downward path connected in the binary tree otherwise return False.
+
+    // In this context downward path means a path that starts at some node and goes downwards.
+
+    // Example 1:
+
+    // Input: head = [4,2,8], root = [1,4,4,null,2,2,null,1,null,6,8,null,null,null,null,1,3]
+    // Output: true
+    // Explanation: Nodes in blue form a subpath in the binary Tree.  
+    // Example 2:
+
+    // Input: head = [1,4,2,6], root = [1,4,4,null,2,2,null,1,null,6,8,null,null,null,null,1,3]
+    // Output: true
+    // Example 3:
+
+    // Input: head = [1,4,2,6,8], root = [1,4,4,null,2,2,null,1,null,6,8,null,null,null,null,1,3]
+    // Output: false
+    // Explanation: There is no path in the binary tree that contains all the elements of the linked list from head.
+
+    // Constraints:
+
+    // The number of nodes in the tree will be in the range [1, 2500].
+    // The number of nodes in the list will be in the range [1, 100].
+    // 1 <= Node.val <= 100 for each node in the linked list and binary tree.
+
+
+    // Solution by me - 1 ms
+    public boolean isSubPath(ListNode head, TreeNode root) {
+        if(root == null) {
+            return false;
+        }
+        if(checkPath(head, root)) {
+            return true;
+        }
+        return isSubPath(head, root.left) || isSubPath(head, root.right);
+    }
+
+    boolean checkPath(ListNode head, TreeNode root) {
+        if(head == null) {
+            return true;
+        }
+        if(root == null || head.val != root.val) {
+            return false;
+        }
+        return checkPath(head.next, root.left) || checkPath(head.next, root.right);
+    }
+
+    // Fastest Solution - 0 ms
+    public boolean isSubPathFastest(ListNode head, TreeNode root) {
+        return dfs(head, head, root);  // Start DFS search
+    }
+
+    // DFS helper function
+    boolean dfs(ListNode head, ListNode cur, TreeNode root) {
+        // If we reach the end of the linked list, return true (successful match)
+        if (cur == null) return true;
+        // If we reach the end of a path in the tree without fully matching the list, return false
+        if (root == null) return false;
+        
+        // Match the current tree node with the current linked list node
+        if (cur.val == root.val) cur = cur.next;
+        // If no match, but the tree node matches the head of the linked list, start a new match
+        else if (head.val == root.val) head = head.next;
+        // Otherwise, reset `cur` to `head` to attempt matching the linked list from scratch
+        else cur = head;
+
+        // Continue DFS down both left and right children
+        return dfs(head, cur, root.left) || dfs(head, cur, root.right);
+    }
+
+
+    // ----------------------------------------------------------------------
+
+
+
+
     // 2807. Insert Greatest Common Divisors in Linked List
     // Solved
     // Medium
