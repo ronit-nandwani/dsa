3136e5a37849d59697df8572664a37bdd7fc1bd3
Ronit Nandwani <ronitnandwani@gmail.com>
2024-06-03 08:08:00 +0100

    Modified: Maths leet code added problem "1518. Water Bottles"

--- advanced/maths/MathsLeetCode.java
+++ advanced/maths/MathsLeetCode.java
@@ -1,6 +1,56 @@
 package advanced.maths;
 
 public class MathsLeetCode {
+    // 1518. Water Bottles
+    // Solved
+    // Easy
+    // Topics
+    // Companies
+    // Hint
+    // There are numBottles water bottles that are initially full of water. You can exchange numExchange empty water bottles from the market with one full water bottle.
+
+    // The operation of drinking a full water bottle turns it into an empty bottle.
+
+    // Given the two integers numBottles and numExchange, return the maximum number of water bottles you can drink.
+
+    // Example 1:
+
+
+    // Input: numBottles = 9, numExchange = 3
+    // Output: 13
+    // Explanation: You can exchange 3 empty bottles to get 1 full water bottle.
+    // Number of water bottles you can drink: 9 + 3 + 1 = 13.
+    // Example 2:
+
+
+    // Input: numBottles = 15, numExchange = 4
+    // Output: 19
+    // Explanation: You can exchange 4 empty bottles to get 1 full water bottle. 
+    // Number of water bottles you can drink: 15 + 3 + 1 = 19.
+    
+
+    // Constraints:
+
+    // 1 <= numBottles <= 100
+    // 2 <= numExchange <= 100
+
+
+    // Solution by me - 0 ms
+    public int numWaterBottles(int numBottles, int numExchange) {
+        int ans = numBottles;
+        int emptyBottles = numBottles;
+        while(emptyBottles>=numExchange) {
+            int v = emptyBottles/numExchange;
+            ans += v;
+            emptyBottles = v + (emptyBottles%numExchange);
+        }
+        return ans;
+    }
+
+
+
+    // -----------------------------------------------------------
+
     // 2582. Pass the Pillow
     // Solved
     // Easy
