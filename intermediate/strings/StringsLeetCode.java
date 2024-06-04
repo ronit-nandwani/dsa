4ad6366240fd3260755b02811adb88c9a5e0c483
Ronit Nandwani <ronitnandwani@gmail.com>
2024-06-04 08:08:00 +0100

    Modified: string leetcode added problem "1684. Count the Number of Consistent Strings"

--- intermediate/strings/StringsLeetCode.java
+++ intermediate/strings/StringsLeetCode.java
@@ -4,6 +4,105 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class StringsLeetCode {
+    
+    // 1684. Count the Number of Consistent Strings
+    // Solved
+    // Easy
+    // Topics
+    // Companies
+    // Hint
+    // You are given a string allowed consisting of distinct characters and an array of strings words. A string is consistent if all characters in the string appear in the string allowed.
+
+    // Return the number of consistent strings in the array words.
+
+    // Example 1:
+
+    // Input: allowed = "ab", words = ["ad","bd","aaab","baa","badab"]
+    // Output: 2
+    // Explanation: Strings "aaab" and "baa" are consistent since they only contain characters 'a' and 'b'.
+    // Example 2:
+
+    // Input: allowed = "abc", words = ["a","b","c","ab","ac","bc","abc"]
+    // Output: 7
+    // Explanation: All strings are consistent.
+    // Example 3:
+
+    // Input: allowed = "cad", words = ["cc","acd","b","ba","bac","bad","ac","d"]
+    // Output: 4
+    // Explanation: Strings "cc", "acd", "ac", and "d" are consistent.
+    
+
+    // Constraints:
+
+    // 1 <= words.length <= 104
+    // 1 <= allowed.length <= 26
+    // 1 <= words[i].length <= 10
+    // The characters in allowed are distinct.
+    // words[i] and allowed contain only lowercase English letters.
+
+
+    // Solution by me - 6 ms
+
+    public int countConsistentStrings(String allowed, String[] words) {
+        // Create a set of allowed characters for quick lookup
+        //Set<Character> allowedSet = new HashSet<>();
+        boolean[] s = new boolean[26];
+        for (char c : allowed.toCharArray()) {
+            //allowedSet.add(c);
+            s[c - 'a'] = true;
+        }
+        
+        int consistentCount = 0;
+        
+        // Check each word in the words array
+        for (String word : words) {
+            boolean isConsistent = true;
+            
+            // Check each character in the word
+            for (char c : word.toCharArray()) {
+                if (!s[c-'a']) {
+                    isConsistent = false;
+                    break;
+                }
+            }
+            
+            // If the word is consistent, increment the count
+            if (isConsistent) {
+                consistentCount++;
+            }
+        }
+        
+        return consistentCount;
+    }
+
+    // Fastest Solution - 4 ms
+
+    public int countConsistentStringsFastest(String allowed, String[] words) {
+        boolean[] s = new boolean[26];
+        for (char c : allowed.toCharArray()) {
+            s[c - 'a'] = true;
+        }
+        int ans = 0;
+        for (String w : words) {
+            if (check(w, s)) {
+                ++ans;
+            }
+        }
+        return ans;
+    }
+
+    private boolean check(String w, boolean[] s) {
+        for (int i = 0; i < w.length(); ++i) {
+            if (!s[w.charAt(i) - 'a']) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // --------------------------------------------
+
+
     // 592. Fraction Addition and Subtraction
     // Solved
     // Medium
