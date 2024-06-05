fe8ffc162d9968dcf6bc4d2b9803af80cc00ca97
Ronit Nandwani <ronitnandwani@gmail.com>
2024-06-05 08:08:00 +0100

    Modified: string leetocde added problem "1598. Crawler Log Folder"

--- intermediate/strings/StringsLeetCode.java
+++ intermediate/strings/StringsLeetCode.java
@@ -4,7 +4,91 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class StringsLeetCode {
+    // 1598. Crawler Log Folder
+    // Solved
+    // Easy
+    // Topics
+    // Companies
+    // Hint
+    // The Leetcode file system keeps a log each time some user performs a change folder operation.
+
+    // The operations are described below:
+
+    // "../" : Move to the parent folder of the current folder. (If you are already in the main folder, remain in the same folder).
+    // "./" : Remain in the same folder.
+    // "x/" : Move to the child folder named x (This folder is guaranteed to always exist).
+    // You are given a list of strings logs where logs[i] is the operation performed by the user at the ith step.
+
+    // The file system starts in the main folder, then the operations in logs are performed.
+
+    // Return the minimum number of operations needed to go back to the main folder after the change folder operations.
+
+    // Example 1:
+
+    // Input: logs = ["d1/","d2/","../","d21/","./"]
+    // Output: 2
+    // Explanation: Use this change folder operation "../" 2 times and go back to the main folder.
+    // Example 2:
+
+    // Input: logs = ["d1/","d2/","./","d3/","../","d31/"]
+    // Output: 3
+    // Example 3:
+
+    // Input: logs = ["d1/","../","../","../"]
+    // Output: 0
     
+
+    // Constraints:
+
+    // 1 <= logs.length <= 103
+    // 2 <= logs[i].length <= 10
+    // logs[i] contains lowercase English letters, digits, '.', and '/'.
+    // logs[i] follows the format described in the statement.
+    // Folder names consist of lowercase English letters and digits.
+
+    // Solution by me - 1 ms
+
+    public int minOperations(String[] logs) {
+        int depth = 0;
+        
+        for (String log : logs) {
+            if (log.equals("../")) {
+                if (depth > 0) {
+                    depth--;
+                }
+            } else if (!log.equals("./")) {
+                depth++;
+            }
+        }
+        
+        return depth;
+    }
+
+    // Fastest Solution - 0 ms
+
+    public int minOperationsFastest(String[] logs) {
+        int d = 0;
+        for(String l:logs){
+           d+=this.getChange(l,d);
+        }
+        return Math.abs(d);
+    }
+
+    private int getChange(String l, int d){
+        if(l.charAt(1)=='.'){
+            if(d>0) return -1;
+        }else{
+            if(l.charAt(0)!='.'){
+                return 1;
+            }
+        }
+        return 0;
+    }
+
+
+    // -----------------------------------------------------------------------------
+
+
     // 1684. Count the Number of Consistent Strings
     // Solved
     // Easy
