72545798a4bd9eca462d5ee40791fb24b7142a63
Ronit Nandwani <ronitnandwani@gmail.com>
2024-06-06 08:08:00 +0100

    Modified: arrays leetcode aded problem "1701. Average Waiting Time"

--- advanced/arrays/ArraysLeetCode.java
+++ advanced/arrays/ArraysLeetCode.java
@@ -32,6 +32,94 @@ class ListNode {
  }
 
 public class ArraysLeetCode {
+    // 1701. Average Waiting Time
+    // Solved
+    // Medium
+    // Topics
+    // Companies
+    // Hint
+    // There is a restaurant with a single chef. You are given an array customers, where customers[i] = [arrivali, timei]:
+
+    // arrivali is the arrival time of the ith customer. The arrival times are sorted in non-decreasing order.
+    // timei is the time needed to prepare the order of the ith customer.
+    // When a customer arrives, he gives the chef his order, and the chef starts preparing it once he is idle. The customer waits till the chef finishes preparing his order. The chef does not prepare food for more than one customer at a time. The chef prepares food for customers in the order they were given in the input.
+
+    // Return the average waiting time of all customers. Solutions within 10-5 from the actual answer are considered accepted.
+
+    // Example 1:
+
+    // Input: customers = [[1,2],[2,5],[4,3]]
+    // Output: 5.00000
+    // Explanation:
+    // 1) The first customer arrives at time 1, the chef takes his order and starts preparing it immediately at time 1, and finishes at time 3, so the waiting time of the first customer is 3 - 1 = 2.
+    // 2) The second customer arrives at time 2, the chef takes his order and starts preparing it at time 3, and finishes at time 8, so the waiting time of the second customer is 8 - 2 = 6.
+    // 3) The third customer arrives at time 4, the chef takes his order and starts preparing it at time 8, and finishes at time 11, so the waiting time of the third customer is 11 - 4 = 7.
+    // So the average waiting time = (2 + 6 + 7) / 3 = 5.
+    // Example 2:
+
+    // Input: customers = [[5,2],[5,4],[10,3],[20,1]]
+    // Output: 3.25000
+    // Explanation:
+    // 1) The first customer arrives at time 5, the chef takes his order and starts preparing it immediately at time 5, and finishes at time 7, so the waiting time of the first customer is 7 - 5 = 2.
+    // 2) The second customer arrives at time 5, the chef takes his order and starts preparing it at time 7, and finishes at time 11, so the waiting time of the second customer is 11 - 5 = 6.
+    // 3) The third customer arrives at time 10, the chef takes his order and starts preparing it at time 11, and finishes at time 14, so the waiting time of the third customer is 14 - 10 = 4.
+    // 4) The fourth customer arrives at time 20, the chef takes his order and starts preparing it immediately at time 20, and finishes at time 21, so the waiting time of the fourth customer is 21 - 20 = 1.
+    // So the average waiting time = (2 + 6 + 4 + 1) / 4 = 3.25.
+    
+
+    // Constraints:
+
+    // 1 <= customers.length <= 105
+    // 1 <= arrivali, timei <= 104
+    // arrivali <= arrivali+1
+
+    // Solution by me - 3 ms
+
+    public double averageWaitingTime(int[][] customers) {
+        long currentTime = 0;
+        long totalWaitingTime = 0;
+
+        for (int[] customer : customers) {
+            int arrival = customer[0];
+            int time = customer[1];
+
+            // The chef starts preparing the order when the customer arrives or when he finishes the previous order, whichever is later.
+            currentTime = Math.max(currentTime, arrival) + time;
+            totalWaitingTime += (currentTime - arrival);
+        }
+
+        return (double) totalWaitingTime / customers.length;
+    }
+
+
+    // Fastest solution - 2 ms
+    public double averageWaitingTimeFastest(int[][] customers) {
+        long totalWait = 0L;
+
+        int shefBusy = 0;
+        for (final int[] customer : customers)
+        {
+            int arriveTime = customer[0];
+            int foodTime = customer[1];
+
+            final int waitTime;
+            if (arriveTime > shefBusy)
+            {
+                waitTime = foodTime;
+                shefBusy = arriveTime + foodTime;
+            } else {
+                waitTime = foodTime + (shefBusy - arriveTime);
+                shefBusy = shefBusy + foodTime;
+            }
+
+            totalWait += waitTime;
+        }
+
+        return (double) totalWait / customers.length;
+    }
+
+
+    // ---------------------------------------------------------
 
     // 2326. Spiral Matrix IV
     // Medium
