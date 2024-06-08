936c9fe89dcd17020c5ed892db3bd02132763c67
Ronit Nandwani <ronitnandwani@gmail.com>
2024-06-08 08:08:00 +0100

    Modified: bit leetcode added problem "2419. Longest Subarray With Maximum Bitwise AND"

--- advanced/bit_manipulation/BitManipulationLeetCode.java
+++ advanced/bit_manipulation/BitManipulationLeetCode.java
@@ -2,6 +2,94 @@ package advanced.bit_manipulation;
 
 public class BitManipulationLeetCode {
 
+    // 2419. Longest Subarray With Maximum Bitwise AND
+    // Solved
+    // Medium
+    // Topics
+    // Companies
+    // Hint
+    // You are given an integer array nums of size n.
+
+    // Consider a non-empty subarray from nums that has the maximum possible bitwise AND.
+
+    // In other words, let k be the maximum value of the bitwise AND of any subarray of nums. Then, only subarrays with a bitwise AND equal to k should be considered.
+    // Return the length of the longest such subarray.
+
+    // The bitwise AND of an array is the bitwise AND of all the numbers in it.
+
+    // A subarray is a contiguous sequence of elements within an array.
+
+    // Example 1:
+
+    // Input: nums = [1,2,3,3,2,2]
+    // Output: 2
+    // Explanation:
+    // The maximum possible bitwise AND of a subarray is 3.
+    // The longest subarray with that value is [3,3], so we return 2.
+    // Example 2:
+
+    // Input: nums = [1,2,3,4]
+    // Output: 1
+    // Explanation:
+    // The maximum possible bitwise AND of a subarray is 4.
+    // The longest subarray with that value is [4], so we return 1.
+    
+
+    // Constraints:
+
+    // 1 <= nums.length <= 105
+    // 1 <= nums[i] <= 106
+
+    // Solution by me - 4 ms
+
+    public int longestSubarray(int[] nums) {
+        int n = nums.length;
+        int len = 1;
+        int maxLen = 1;
+        int max = nums[0];
+        for(int i=1;i<n;i++) {
+            if(nums[i] > max) {
+                max = nums[i];
+                len = 1;
+                maxLen = 1;
+            } else if(nums[i] == max) {
+                len++;
+            } else if(nums[i] < max) {                
+                len = 0;
+            }
+            maxLen = Math.max(len, maxLen);
+        }
+        return maxLen;
+    }
+
+    // Fastest Solution - 2 ms
+
+    public int longestSubarrayFastest(int[] a) {
+        int n=a.length;
+        int max=0;
+        int c=0,c1=0;
+        for(int i=0;i<n;i++)
+        {
+            if(max<a[i])
+            max=a[i];
+        }
+        for(int i=0;i<n;i++)
+        {
+            if(max==a[i])
+            {
+                c1++;
+                c=Math.max(c,c1);
+            }
+                else
+                c1=0;
+            
+        }
+        return c;
+        
+    }
+
+    // ----------------------------------------------------------
+
     // 2220. Minimum Bit Flips to Convert Number
     // Solved
     // Easy
