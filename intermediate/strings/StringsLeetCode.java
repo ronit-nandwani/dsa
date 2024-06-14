5d3ef28ee1f4f29f24f6ee8aeb0a5a539ccb18d1
Ronit Nandwani <ronitnandwani@gmail.com>
2024-06-14 08:08:00 +0100

    Modified: Strings leetcode added problem "214. Shortest Palindrome"

--- intermediate/strings/StringsLeetCode.java
+++ intermediate/strings/StringsLeetCode.java
@@ -11,6 +11,84 @@ import java.util.TreeMap;
 import advanced.trees.Trees2.Pair;
 
 public class StringsLeetCode {
+    // 214. Shortest Palindrome
+    // Solved
+    // Hard
+    // Topics
+    // Companies
+    // You are given a string s. You can convert s to a 
+    // palindrome
+    // by adding characters in front of it.
+
+    // Return the shortest palindrome you can find by performing this transformation.
+
+    // Example 1:
+
+    // Input: s = "aacecaaa"
+    // Output: "aaacecaaa"
+    // Example 2:
+
+    // Input: s = "abcd"
+    // Output: "dcbabcd"
+    
+    // Constraints:
+
+    // 0 <= s.length <= 5 * 104
+    // s consists of lowercase English letters only.
+
+    // Brute force solution - O(n^2) - 220 ms
+
+    public String shortestPalindrome(String s) {
+        int n = s.length();
+         String rev = new StringBuilder(s).reverse().toString();
+         
+         // Try each prefix of the original string as a palindrome
+         for (int i = n; i >= 0; i--) {
+             if (s.substring(0, i).equals(rev.substring(n - i))) {
+                 return rev.substring(0, n - i) + s;
+             }
+         }
+         return "";
+     }
+
+     // Fastest Solution - 5 ms
+
+     public String shortestPalindromeFastest(String s) {
+        int length = s.length();
+        if (length == 0) {
+            return s;
+        }
+
+        // Find the longest palindromic prefix
+        int left = 0;
+        for (int right = length - 1; right >= 0; right--) {
+            if (s.charAt(right) == s.charAt(left)) {
+                left++;
+            }
+        }
+
+        // If the whole string is a palindrome, return the original string
+        if (left == length) {
+            return s;
+        }
+
+        // Extract the suffix that is not part of the palindromic prefix
+        String nonPalindromeSuffix = s.substring(left);
+        StringBuilder reverseSuffix = new StringBuilder(
+            nonPalindromeSuffix
+        ).reverse();
+
+        // Form the shortest palindrome by prepending the reversed suffix
+        return reverseSuffix
+            .append(shortestPalindrome(s.substring(0, left)))
+            .append(nonPalindromeSuffix)
+            .toString();
+    }
+
+
+    // ----------------------------------------------------------
+
+
     // 539. Minimum Time Difference
     // Solved
     // Medium
