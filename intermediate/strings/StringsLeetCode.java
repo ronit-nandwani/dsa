b10d03d7962c602076b80b08cae8c0e1099db91c
Ronit Nandwani <ronitnandwani@gmail.com>
2024-06-19 08:08:00 +0100

    Modified: string leetcode added problem "567. Permutation in String"

--- intermediate/strings/StringsLeetCode.java
+++ intermediate/strings/StringsLeetCode.java
@@ -11,6 +11,104 @@ import java.util.TreeMap;
 import advanced.trees.Trees2.Pair;
 
 public class StringsLeetCode {
+    // 567. Permutation in String
+    // Solved
+    // Medium
+    // Topics
+    // Companies
+    // Hint
+    // Given two strings s1 and s2, return true if s2 contains a 
+    // permutation
+    // of s1, or false otherwise.
+
+    // In other words, return true if one of s1's permutations is the substring of s2.
+
+    // Example 1:
+
+    // Input: s1 = "ab", s2 = "eidbaooo"
+    // Output: true
+    // Explanation: s2 contains one permutation of s1 ("ba").
+    // Example 2:
+
+    // Input: s1 = "ab", s2 = "eidboaoo"
+    // Output: false
+    
+
+    // Constraints:
+
+    // 1 <= s1.length, s2.length <= 104
+    // s1 and s2 consist of lowercase English letters.
+
+
+    // Solution - 73 ms
+
+    public boolean checkInclusion(String s1, String s2) {
+        if (s1.length() > s2.length())
+            return false;
+        int[] s1arr = new int[26];
+        for (int i = 0; i < s1.length(); i++)
+            s1arr[s1.charAt(i) - 'a']++;
+        for (int i = 0; i <= s2.length() - s1.length(); i++) {
+            int[] s2arr = new int[26];
+            for (int j = 0; j < s1.length(); j++) {
+                s2arr[s2.charAt(i + j) - 'a']++;
+            }
+            if (matches(s1arr, s2arr))
+                return true;
+        }
+        return false;
+    }
+    
+    public boolean matches(int[] s1arr, int[] s2arr) {
+        for (int i = 0; i < 26; i++) {
+            if (s1arr[i] != s2arr[i])
+                return false;
+        }
+        return true;
+    }
+
+    // Fastest Solution - 9 ms
+
+    public boolean checkInclusionFastest(String s1, String s2) {
+        if (s1.length() > s2.length())
+            return false;
+        int[] s1arr = new int[26];
+        int[] s2arr = new int[26];
+        for (int i = 0; i < s1.length(); i++) {
+            s1arr[s1.charAt(i) - 'a']++;
+            s2arr[s2.charAt(i) - 'a']++;
+        }
+
+        int count = 0;
+        for (int i = 0; i < 26; i++) {
+            if (s1arr[i] == s2arr[i])
+                count++;
+        }
+
+        for (int i = 0; i < s2.length() - s1.length(); i++) {
+            int r = s2.charAt(i + s1.length()) - 'a', l = s2.charAt(i) - 'a';
+            if (count == 26)
+                return true;
+            s2arr[r]++;
+            if (s2arr[r] == s1arr[r]) {
+                count++;
+            } else if (s2arr[r] == s1arr[r] + 1) {
+                count--;
+            }
+            s2arr[l]--;
+            if (s2arr[l] == s1arr[l]) {
+                count++;
+            } else if (s2arr[l] == s1arr[l] - 1) {
+                count--;
+            }
+        }
+        return count == 26;
+    }
+
+
+    // ------------------------------------------------------------------
+
+
     // 214. Shortest Palindrome
     // Solved
     // Hard
