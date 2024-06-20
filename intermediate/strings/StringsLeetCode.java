afdd17b0523c29863e8932983f642c2935891a09
Ronit Nandwani <ronitnandwani@gmail.com>
2024-06-20 08:08:00 +0100

    Modified: strings leetcode added problem "1813. Sentence Similarity III"

--- intermediate/strings/StringsLeetCode.java
+++ intermediate/strings/StringsLeetCode.java
@@ -1,7 +1,10 @@
 package intermediate.strings;
 
+import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.Deque;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -11,6 +14,115 @@ import java.util.TreeMap;
 import advanced.trees.Trees2.Pair;
 
 public class StringsLeetCode {
+    // 1813. Sentence Similarity III
+    // Medium
+    // Topics
+    // Companies
+    // Hint
+    // You are given two strings sentence1 and sentence2, each representing a sentence composed of words. A sentence is a list of words that are separated by a single space with no leading or trailing spaces. Each word consists of only uppercase and lowercase English characters.
+
+    // Two sentences s1 and s2 are considered similar if it is possible to insert an arbitrary sentence (possibly empty) inside one of these sentences such that the two sentences become equal. Note that the inserted sentence must be separated from existing words by spaces.
+
+    // For example,
+
+    // s1 = "Hello Jane" and s2 = "Hello my name is Jane" can be made equal by inserting "my name is" between "Hello" and "Jane" in s1.
+    // s1 = "Frog cool" and s2 = "Frogs are cool" are not similar, since although there is a sentence "s are" inserted into s1, it is not separated from "Frog" by a space.
+    // Given two sentences sentence1 and sentence2, return true if sentence1 and sentence2 are similar. Otherwise, return false.
+
+    // Example 1:
+
+    // Input: sentence1 = "My name is Haley", sentence2 = "My Haley"
+
+    // Output: true
+
+    // Explanation:
+
+    // sentence2 can be turned to sentence1 by inserting "name is" between "My" and "Haley".
+
+    // Example 2:
+
+    // Input: sentence1 = "of", sentence2 = "A lot of words"
+
+    // Output: false
+
+    // Explanation:
+
+    // No single sentence can be inserted inside one of the sentences to make it equal to the other.
+
+    // Example 3:
+
+    // Input: sentence1 = "Eating right now", sentence2 = "Eating"
+
+    // Output: true
+
+    // Explanation:
+
+    // sentence2 can be turned to sentence1 by inserting "right now" at the end of the sentence.
+
+    // Constraints:
+
+    // 1 <= sentence1.length, sentence2.length <= 100
+    // sentence1 and sentence2 consist of lowercase and uppercase English letters and spaces.
+    // The words in sentence1 and sentence2 are separated by a single space.
+
+
+
+    // Other solution - 2 ms
+
+    public boolean areSentencesSimilar(String s1, String s2) {
+        Deque<String> deque1 = new ArrayDeque<>(Arrays.asList(s1.split(" ")));
+        Deque<String> deque2 = new ArrayDeque<>(Arrays.asList(s2.split(" ")));
+        //Compare the prefixes or beginning of the strings.
+        while (
+            !deque1.isEmpty() &&
+            !deque2.isEmpty() &&
+            deque1.peek().equals(deque2.peek())
+        ) {
+            deque1.poll();
+            deque2.poll();
+        }
+        // Compare the suffixes or ending of the strings.
+        while (
+            !deque1.isEmpty() &&
+            !deque2.isEmpty() &&
+            deque1.peekLast().equals(deque2.peekLast())
+        ) {
+            deque1.pollLast();
+            deque2.pollLast();
+        }
+        return deque1.isEmpty() || deque2.isEmpty();
+    }
+
+    // Fastest Solution - 1 ms(2 pointer approach)
+
+    public boolean areSentencesSimilarFastest(String sentence1, String sentence2) {
+        String[] words1 = sentence1.split(" ");
+        String[] words2 = sentence2.split(" ");
+        
+        int i = 0; // pointer for prefix
+        int j = 0; // pointer for suffix
+        
+        // Find the common prefix
+        while (i < words1.length && i < words2.length && words1[i].equals(words2[i])) {
+            i++;
+        }
+        
+        // Find the common suffix
+        while (j < words1.length - i && j < words2.length - i && 
+               words1[words1.length - 1 - j].equals(words2[words2.length - 1 - j])) {
+            j++;
+        }
+        
+        // If the remaining words in the longer sentence can be removed
+        return i + j == Math.min(words1.length, words2.length);
+    }
+
+
+
+
+    // ----------------------------------------
+
+
     // 567. Permutation in String
     // Solved
     // Medium
